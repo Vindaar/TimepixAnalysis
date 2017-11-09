dfc7d410a0e48581e3349ed9a40290872fcaf5b5
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2017-11-09 19:27:08 +0100

    add end_time_extractor function to work on run folders (w/o rm after)

--- end_time_extractor.nim
+++ end_time_extractor.nim
@@ -20,6 +20,10 @@ proc main() =
 
   # first check whether the input really is a .tar.gz file
   let is_tar = ".tar.gz" in input_file
+
+  # we need a variable for the run folder in order to deal with both cases
+  # .tar.gz and run folder as input
+  var run_folder = ""
   
   if is_tar:
     # in this case we need to extract the file to a temp directory
@@ -28,37 +32,46 @@ proc main() =
     # .tar, which we need to remove
     let name = split(name_tar, ".tar")[0]
     # given the directory, make system call to tar and extract the folder
-    var cmd = "tar -xzf " & input_file & " --directory " & dir
-    echo "System call to tar:\n\t", cmd
-    var (x, y) = execCmdEx(cmd)
-    if y == 0:
+    let cmd_tar = "tar -xzf " & input_file & " --directory " & dir
+    echo "System call to tar:\n\t", cmd_tar
+    var (x, y) = execCmdEx(cmd_tar)
+    if y != 0:
+      echo "Warning: the extraction failed with exit status: x = ", x, " y = ", y
+      quit()
+    else:
       # in this case tar returned 0 (== success)
       # now that we have extracted the folder, get list of files in run folder
-      let run_folder = joinPath(dir, name)
-      let files = getListOfFiles(run_folder, r"^.*data\d\d\d\d\d\d\.txt$")
-      if len(files) == 0:
-        echo "Either folder ", run_folder, " does not exist, or it is empty."
-        quit()
-      echo len(files)
+      run_folder = joinPath(dir, name)
+  else:
+    run_folder = paramStr(1)
+    
+  # now we have a run folder, which we can work on
+  let files = getListOfFiles(run_folder, r"^.*data\d\d\d\d\d\d\.txt$")
+  if len(files) == 0:
+    echo "Either folder ", run_folder, " does not exist, or it is empty."
+    quit()
+  echo len(files)
 
+  let rt_info = getRunTimeInfo(files)
 
-      let rt_info = getRunTimeInfo(files)
+  let parsed_first = formatAsOrgDate(rt_info.t_start)
+  let parsed_last  = formatAsOrgDate(rt_info.t_end)      
+  
+  echo "Start of run:  <", parsed_first, ">"
+  echo "End of run:    <", parsed_last, ">"
+  echo "Length of run: ", rt_info.t_length.days, " days ", rt_info.t_length.hours, ":", rt_info.t_length.minutes
+  
+  if is_tar:
+    # in this case we need to remove the temp files again
+    # now that we have all information we needed from the run, we can delete the folder again
+    let cmd_rm = "rm -rf " & run_folder
+    echo "System call to rm:\n\t", cmd_rm
+    let (x, y) = execCmdEx(cmd_rm)
+    if y == 0:
+      echo "... removed temp files"
+    else:
+      echo "Warning: something went wrong during deletion of temp files. x = ", x, " y =", y
 
-      let parsed_first = formatAsOrgDate(rt_info.t_start)
-      let parsed_last  = formatAsOrgDate(rt_info.t_end)      
-      
-      echo "Start of run:  <", parsed_first, ">"
-      echo "End of run:    <", parsed_last, ">"
-      echo "Length of run: ", rt_info.t_length.days, " days ", rt_info.t_length.hours, ":", rt_info.t_length.minutes
-      
-      # now that we have all information we needed from the run, we can delete the folder again
-      cmd = "rm -rf " & run_folder
-      echo "System call to rm:\n\t", cmd
-      (x, y) = execCmdEx(cmd)
-      if y == 0:
-        echo "... removed temp files"
-      else:
-        echo "Warning: something went wrong during deletion of temp files. x = ", x, " y =", y
   
 
 when isMainModule:

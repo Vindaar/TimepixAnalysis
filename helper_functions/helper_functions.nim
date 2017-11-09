bcfac2744d6343efaea093e7994396186e4251d2
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2017-11-09 14:50:20 +0100

    add creation of inode table to helper_functions and add sortInodeTab

--- helper_functions/helper_functions.nim
+++ helper_functions/helper_functions.nim
@@ -27,3 +27,34 @@ proc mean*[T](array: seq[T]): float =
   result = foldl(array, a + b)
   result = result / n_elements
 
+
+proc sortInodeTable*(inode_table: OrderedTable[int, string]): OrderedTable[int, string] =
+  # this procedure sorts the given inode table by inode, to provide faster read
+  # speeds
+  # inputs:
+  #   inode_table: OrderedTable[int, string] which contains a filename together with the
+  #     corresponding inode
+
+  # this one liner uses the sorted() to create a sorted copy of inode_table. It uses 
+  # system.cmp[int] (the int comparison procedure) to compare the inodes
+  result = sorted(inode_table, proc(x, y: (int, string)): int = system.cmp[int](x[0], y[0]))
+
+
+proc createInodeTable*(list_of_files: seq[string]): OrderedTable[int, string] =
+  # returns a table containing filenames and Inode number
+  result = initOrderedTable[int, string]()
+  # functional way to create table of inodes
+  result = toOrderedTable(
+    map(
+      list_of_files, 
+      # the following line is the normal way to write the line after it
+      # proc(n: string): (string, int) = (n, int(getFileInfo(n).id.file))
+      # read line as: take string 'n' and make a tuple (string, int) from it
+      # by applying getFileInfo to n and taking the file id
+      (n: string) -> (int, string) => (int(getFileInfo(n).id.file), n)
+  ))
+  # and normal procedural way to create list of inodes
+  # for file in list_of_files:
+  #   let ino = int(getFileInfo(file).id.file)
+  #   result[file] = ino
+
--- helper_functions/helper_functions.nimble
+++ helper_functions/helper_functions.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.0"
+version       = "0.1.1"
 author        = "Vindaar"
 description   = "A simple collection of functions regarding arrays and sequences, which are useful and not part of the standard library."
 license       = "MIT"

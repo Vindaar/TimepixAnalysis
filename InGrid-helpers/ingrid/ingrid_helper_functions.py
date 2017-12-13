966915de6be9c6a284ed08c0d81011816f99d29a
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2017-12-13 17:01:43 +0100

    refactor reading of files, switch to argparse and int. FADC spec.
    - refactors the reading of different column based files files.
    - switches to command line arg parsing using argparse
    - adds the ingrid_helper_functions python module, containig functions
    - to read single column data files (e.g. Hits, ToTs etc.)
    - adds option to also read and plot FADC spectra, making FADC spectra
    - plotting script redundant
    - make fancy LaTeX text optional

---
+++ InGrid-helpers/ingrid/ingrid_helper_functions.py
@@ -0,0 +1,21 @@
+# a combination of some useful functions dealing with InGrid related helper functions
+# mostly related to plotting data, which was produced by Nim
+
+import numpy as np
+
+def readFileColumn(filename):
+    lines = open(filename, "r").readlines()
+    data = []
+    for line in lines:
+        if "#" not in line:
+            data.append(float(line))
+    return np.asarray(data)
+    
+def readFadcSpectrumFile(filename):
+    return readFileColumn(filename)
+
+def readHitsFile(filename):
+    return readFileColumn(filename)
+
+def readTotsFile(filename):
+    return readFileColumn(filename)

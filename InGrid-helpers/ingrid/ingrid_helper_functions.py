3f06e6125c2dcea66351b498d2dd2b2ac83a5061
Sebastian Schmidt <basti90@gmail.com>
2018-01-15 12:41:31 +0100

    add script to fit Fe spectrum and plot H5 data

--- InGrid-helpers/ingrid/ingrid_helper_functions.py
+++ InGrid-helpers/ingrid/ingrid_helper_functions.py
@@ -2,6 +2,8 @@
 # mostly related to plotting data, which was produced by Nim
 
 import numpy as np
+import h5py
+import os
 
 def readFileColumn(filename):
     lines = open(filename, "r").readlines()
@@ -19,3 +21,46 @@ def readHitsFile(filename):
 
 def readTotsFile(filename):
     return readFileColumn(filename)
+
+
+def recoDataChipBase(run_number):
+    return "/reconstruction/run_{}/chip_".format(run_number)
+
+def readH5Data(h5file, group_name, dset_names):
+    # given a H5 file, a run number one, a chip number
+    # and one ore more dataset names, read the data and return
+    # a list of the `dset_names` arrays
+
+    print("Opening h5file {}".format(h5file))
+
+    h5f = h5py.File(h5file, "r")
+    print(h5f)
+    print("Reading group {}".format(group_name))
+    group = h5f[group_name]
+    print(group)
+    result = []
+    for dset in dset_names:
+        print("Opening dset {}".format(dset))
+        dset_h5 = group[dset]
+        result.append(dset_h5[:])
+        
+    h5f.close()
+    return result
+
+
+def writeFitParametersH5(h5file, fit_results, group_name, dset_name):
+    # writes the fit results to the H5 file given
+    # currently only writing the energy calibration
+    # as the results for the spectrum are not actully used in the
+    # reconstruction or analysis
+    h5f = h5py.File(h5file, "w")
+    group = h5f[group_name]
+    dset_h5 = group[dset_name]
+
+    popt, pcov, popt_E, pcov_E = fit_results
+
+    dset_h5.attrs["eV_per_pix"] = popt_E[0]
+    dset_h5.attrs["d_eV_per_pix"] = np.sqrt(pcov_E[0][0])
+
+    h5f.close()
+    

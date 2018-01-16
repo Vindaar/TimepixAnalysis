986806b873103b8949625a41bf900f5622c0872a
Sebastian Schmidt <basti90@gmail.com>
2018-01-16 11:29:37 +0100

    clean up plotting of data, provide stacked plotting

--- InGrid-helpers/ingrid/ingrid_helper_functions.py
+++ InGrid-helpers/ingrid/ingrid_helper_functions.py
@@ -4,6 +4,7 @@
 import numpy as np
 import h5py
 import os
+import matplotlib.pyplot as plt
 
 def readFileColumn(filename):
     lines = open(filename, "r").readlines()
@@ -22,19 +23,60 @@ def readHitsFile(filename):
 def readTotsFile(filename):
     return readFileColumn(filename)
 
+def recoBase():
+    return "/reconstruction/"
+
 def recoDataChipBase(run_number):
     return "/reconstruction/run_{}/chip_".format(run_number)
 
-def readH5Data(h5file, group_name, dset_names):
-    # given a H5 file, a run number one, a chip number
-    # and one ore more dataset names, read the data and return
-    # a list of the `dset_names` arrays
+def binData(data, cuts):
+    if cuts is not None:
+      for cut_str in cuts:
+          # careful, this is somewhat dangerous, because we just evaluate any string
+          # this could do something evil!
+          cut = eval(cut_str)
+          data = data[np.where(cut)[0]]
+          print data
 
-    print("Opening h5file {}".format(h5file))
+    # assume binsize of 1 for now
+    data = np.concatenate(data).flatten()
+    binning = np.linspace(-0.5, np.max(data) + 0.5, np.max(data) + 2)
+    
+    hist, bin_edges = np.histogram(data, binning)
+    bins = np.arange(np.max(data) + 1)
 
-    h5f = h5py.File(h5file, "r")
-    print(h5f)
+    # return data as tuple (bin content / binning)
+    # we remove the last element due to the way we create the bins
+    return (hist, bin_edges[:-1])
+
+def plotData(hist, binning, outfile, title, xlabel, ylabel, save_plot = True):
+    fig, ax = plt.subplots(1, 1)
+    #ax.hist(data, bins = 199)
+    if binning is None:
+        ax.hist(hist, 500)
+    else:
+        print(np.shape(hist), np.shape(binning))
+        ax.bar(binning, hist, 1., align='edge', linewidth=0.2)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    #ax.set_xlim(0, np.percentile(data, 98))
+    outname = outfile + ".pdf"
+
+    if save_plot == True:
+        plt.savefig(outname)
+        plt.show()
+        return None
+    else:
+        return fig, ax
+
+def readH5DataSingle(h5f, group_name, dset_names):
+    # proc which reads datasets from the a single group
+    # with name `dset_name` in the already opened
+    # file h5file
     print("Reading group {}".format(group_name))
+    print("Reading dset {}".format(dset_names))
     group = h5f[group_name]
     print(group)
     result = []
@@ -42,6 +84,30 @@ def readH5Data(h5file, group_name, dset_names):
         print("Opening dset {}".format(dset))
         dset_h5 = group[dset]
         result.append(dset_h5[:])
+    return result
+
+def readH5Data(h5file, group_name, chip, dset_names):
+    # given a H5 file, a run number one, a chip number
+    # and one ore more dataset names, read the data and return
+    # a list of the `dset_names` arrays
+
+    print("Opening h5file {}".format(h5file))
+    h5f = h5py.File(h5file, "r")
+    print(h5f)
+    
+    # first check whether we read one run or all
+    result = []
+    if group_name == recoBase():
+        basegroup = h5f[group_name]
+        # in this case have to iterate over all runs
+        for grp in basegroup.keys():
+            print "Reading group ", grp
+            if "run_" in grp:
+                result.append(readH5DataSingle(h5f, recoBase() + grp + "/chip_{}".format(chip), dset_names))
+                print result
+        result = np.asarray(result).flatten()
+    else:
+        result = readH5DataSingle(h5f, group_name, dset_names)
         
     h5f.close()
     return result
--- helper_scripts/PyS_plotH5data.py
+++ helper_scripts/PyS_plotH5data.py
@@ -13,6 +13,8 @@ from scipy.optimize import curve_fit
 
 import numpy as np
 
+from fit_fe_spectrum import *
+
 
 def fancy_plotting():
     # set up some LaTeX plotting parameters
@@ -40,281 +42,10 @@ def fancy_plotting():
               'figure.figsize':      fig_size}
     pylab.rcParams.update(params)
 
-def binData(data, cuts):
-    if cuts is not None:
-      for cut_str in cuts:
-          # careful, this is somewhat dangerous, because we just evaluate any string
-          # this could do something evil!
-          cut = eval(cut_str)
-          data = data[np.where(cut)[0]]
-          print data
-
-    # assume binsize of 1 for now
-    binning = np.linspace(-0.5, np.max(data) + 0.5, np.max(data) + 2)
-    hist, bin_edges = np.histogram(data, binning)
-    bins = np.arange(np.max(data) + 1)
-
-    # return data as tuple (bin content / binning)
-    # we remove the last element due to the way we create the bins
-    return (hist, bin_edges[:-1])
-
-def plotData(hist, binning, outfile, title, xlabel, ylabel, save_plot = True):
-    fig, ax = plt.subplots(1, 1)
-    #ax.hist(data, bins = 199)
-    print(np.shape(hist), np.shape(binning))
-    ax.bar(binning, hist, 1., align='edge', linewidth=0.2)
-    if title is not None:
-        ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    #ax.set_xlim(0, np.percentile(data, 98))
-    outname = outfile + ".pdf"
-
-    if save_plot == True:
-        plt.savefig(outname)
-        plt.show()
-        return None
-    else:
-        return fig, ax
-
-# this whole fit is probably going to be pretty slow.
-# convert to Nim functions, which we compile to lib and call
-# in python via ctypes
-# see: https://akehrer.github.io/posts/connecting-nim-to-python/
-# given the good starting parameters and that we only have to fit
-# /once/ per calibration run, this is not a priorty for now, although
-# it would be fun :)
-def gauss(x, mean, sigma, norm = False):
-    # based on the ROOT implementation of TMath::Gaus:
-    # https://root.cern.ch/root/html524/src/TMath.cxx.html#dKZ4iB
-    if sigma == 0:
-        return 1.e30
-    arg = (x-mean)/sigma;
-    res = np.exp(-0.5 * arg * arg)
-    if norm == False:
-        return res
-    else:
-        return res / (2.50662827463100024 * sigma) # sqrt(2*Pi)=2.5066282746310002
-
-def expGauss(x_ar, p):
-    # exponential * times (?!) from Christoph's expogaus.c
-    if len(p) != 5:
-        return np.nan
-    p_val = 2 * (p[1] * np.power(p[4], 2) - p[3])
-    q_val = 2 * np.power(p[4], 2) * p[0] + np.power(p[3], 2) - np.log(p[2]) * 2 * np.power(p[4], 2)
-
-    threshold = - p_val / 2.0 - np.sqrt( np.power(p_val, 2) / 4.0 - q_val )
-
-    x_out = np.array(x_ar, copy = True)
-    for i, x in enumerate(x_ar):
-        if x < threshold:
-            x_out[i] = np.exp(p[0] + p[1] * x)
-        else:
-            x_out[i] = p[2] * gauss(x, p[3], p[4])
-
-    return x_out
-
-def feSpectrumFunc(x, *p_ar):#p_ar):
-    # NOTE: should we init with 0? or 1?
-
-    parAll = np.zeros(20, dtype = np.float)
-    if len(p_ar) != 15:
-        print("Lenght of params is ", len(p_ar))
-        import sys
-        sys.exit("Bad parameter set! Sorry for killing the progam :)")
-
-    # while this is the actual fitting function, it is in fact only
-    # a wrapper around 4 Gaussians * exponential decay
-    # It's a mixture of the K_alpha, K_beta lines as well as
-    # the escape peaks
-    # see fitFeSpectrum() (where bounds are set) for discussion of different
-    # parameters
-    parAll[0] = p_ar[0]
-    parAll[1] = p_ar[1]
-    parAll[2] = p_ar[2]
-    parAll[3] = p_ar[3]
-    parAll[4] = p_ar[4]
-
-    parAll[5] = p_ar[5]
-    parAll[6] = p_ar[6]
-    parAll[7] = p_ar[14]*p_ar[2]
-    parAll[8] = 3.5/2.9*p_ar[3]
-    parAll[9] = p_ar[4]
-
-    parAll[10] = p_ar[7]
-    parAll[11] = p_ar[8]
-    parAll[12] = p_ar[9]
-    parAll[13] = p_ar[10]
-    parAll[14] = p_ar[11]
-
-    parAll[15] = p_ar[12]
-    parAll[16] = p_ar[13]
-    parAll[17] = p_ar[14]*p_ar[9]
-    parAll[18] = 6.35/5.75*p_ar[10]
-    parAll[19] = p_ar[11]
-
-    value = 0
-    value += expGauss(x, parAll[0:5])
-    value += expGauss(x, parAll[5:10])
-    value += expGauss(x, parAll[10:15])
-    value += expGauss(x, parAll[15:20])
-
-    return value
-
-def fitFeSpectrum(hist, binning, cuts):
-    # given our histogram and binning data
-    # for fit := (y / x) data
-    # fit a double gaussian to the data
-
-    # define center, std and amplitude of K_alpha line
-    # as well as escape peak
-    mu_kalpha = np.argmax(hist)
-    sigma_kalpha = mu_kalpha / 10.0
-    n_kalpha = hist[mu_kalpha]
-    print mu_kalpha
-    print sigma_kalpha
-    print n_kalpha
-    mu_kalpha_esc = mu_kalpha * 2.9/5.75
-    sigma_kalpha_esc = mu_kalpha_esc / 10.0
-    n_kalpha_esc = n_kalpha / 10.0
-
-    params = np.zeros(15, dtype = np.float)
-    params[2] = n_kalpha_esc
-    params[3] = mu_kalpha_esc
-    params[4] = sigma_kalpha_esc
-
-    params[9] = n_kalpha
-    params[10] = mu_kalpha
-    params[11] = sigma_kalpha
-
-    params[14] = 17.0 / 150.0
-
-    l_bounds = []
-    u_bounds = []
-    for i in xrange(15):
-        l_bounds.append(-np.inf)
-        u_bounds.append(np.inf)
-    # set bound on paramerters
-    # constrain amplitude of K_beta to some positive value    
-    l_bounds[2] = 0
-    u_bounds[2] = 10000
-    # constrain amplitude of K_alpha to some positive value
-    l_bounds[9] = 0
-    u_bounds[9] = 10000
-
-    # location of K_alpha escape peak, little more than half of K_alpha location
-    l_bounds[3] = mu_kalpha_esc * 0.8
-    u_bounds[3] = mu_kalpha_esc * 1.2
-
-    # bounds for center of K_alpha peak (should be at around 220 electrons, hits)
-    l_bounds[10] = mu_kalpha*0.8
-    u_bounds[10] = mu_kalpha*1.2
-
-    # some useful bounds for K_alpha escape peak width
-    l_bounds[4] = sigma_kalpha_esc*0.5
-    u_bounds[4] = sigma_kalpha_esc*1.5
-
-    # some useful bounds for K_alpha width
-    l_bounds[11] = sigma_kalpha*0.5
-    u_bounds[11] = sigma_kalpha*1.5
-    # param 14: "N_{K_{#beta}}/N_{K_{#alpha}}"
-    # known ratio of two K_alpha and K_beta, should be in some range
-    l_bounds[14] = 0.01
-    u_bounds[14] = 0.3
-
-    # this leaves parameter 7 and 8, as well as 12 and 13 without bounds
-    # these describe the exponential factors contributing, since they will
-    # be small anyways...
-    bounds = (l_bounds, u_bounds)
-    print(len(bounds))
-    print bounds
-    print(len(params))
-
-    # only fit in range up to 350 hits. Can take index 350 on both, since we
-    # created the histogram for a binning with width == 1 pixel per hit
-    data_tofit = hist[0:350]
-    bins_tofit = binning[0:350]
-    #print data_tofit
-    #print bins_tofit
-    lb, ub = [np.asarray(b, dtype=float) for b in bounds]
-    print ub
-    
-    result = curve_fit(feSpectrumFunc, bins_tofit, data_tofit, p0=params, bounds = bounds)#, full_output=True)
-    popt = result[0]
-    pcov = result[1]
-    # Calculate the reduced Chi^2:
-    # n_dof: # degrees of freedom
-    #n_dof  = (np.size(infodict[0]['fvec']) - np.size(popt))
-    #chi_sq = np.sum(infodict[0]['fvec']**2) / n_dof
-    print '--------------------------------------------------'
-    print 'Parameters of calibration fit: '
-    for i, p in enumerate(popt):
-        print 'p_{} ='.format(i), popt[i], '+-', np.sqrt(pcov[i][i])
-    #print 'Chi^2 / dof =', chi_sq
-
-    print "yay :)"
-
-    return popt, pcov
-
-def linear_func(x, a):
-    return a * x
-
-def fitAndPlotFeSpectrum(data, cuts, outfolder, run_number, fitting_only = False):
-
-    # bin the data
-    hist, binning = binData(data, cuts)
-
-    popt, pcov = fitFeSpectrum(hist, binning, cuts)
-
-    # now get some nice x / y points from the fit parameters
-    # given these values, plot
-    x_pl = np.linspace(0, 350, 3500)
-    y_pl = feSpectrumFunc(x_pl, *popt)
-
-    fig, ax = plotData(hist, binning, "", "Fe spectrum", "\\# pixels hit", "\\# events", False)
-
-    # TODO: add fit parameter results to plots as legend!
-
-    ax.set_xlim(0, 350)
-    ax.plot(x_pl, y_pl, linewidth = 2, color = "red")
-    plt.savefig(os.path.join(outfolder, "fe_spectrum_{}.pdf".format(run_number)))
-    if fitting_only == False:
-        plt.show()
-
-    # now we can fit the energy calibration function
-    energies = [2.925, 5.755]
-    pixels_peaks = [popt[3], popt[10]]
-    pixels_err = [np.sqrt(pcov[3][3]), np.sqrt(pcov[10][10])]
-    result = curve_fit(linear_func, energies, pixels_peaks, sigma = pixels_err, full_output=True)
-    popt_E = result[0]
-    pcov_E = result[1]
-    infodict = result[2:]
-    # n_dof: # degrees of freedom
-    n_dof  = (np.size(infodict[0]['fvec']) - np.size(popt_E))
-    chi_sq = np.sum(infodict[0]['fvec']**2) / n_dof
-    print '--------------------------------------------------'
-    print 'Parameters of calibration fit: '
-    for i, p in enumerate(popt_E):
-        print 'p_{} ='.format(i), popt_E[i], '+-', np.sqrt(pcov_E[i][i])
-    a_inv = 1.0 / popt_E[0] * 1000
-    da_inv = a_inv * np.sqrt(pcov_E[0][0]) / popt_E[0]
-    print("a^-1 = {0} +- {1}".format(a_inv, da_inv))
-    print 'Chi^2 / dof =', chi_sq
-                                       
-    E_calc = np.linspace(2, 7, 1000)
-    H_calc = linear_func(E_calc, popt_E[0])
-    plt.errorbar(energies, pixels_peaks, yerr = pixels_err, marker = ".", markersize = 8, linestyle = "", color = "red")
-    plt.plot(E_calc, H_calc, marker = "", linestyle = "-")
-    plt.xlabel("Energy / keV")
-    plt.ylabel("# pixels hit")
-    plt.title("Energy calibration function based on # pix in Fe55 spectrum")
-    plt.grid()
-    plt.savefig(os.path.join(outfolder, "fe_energy_calibration_{}.pdf".format(run_number)))
-    if fitting_only == False:
-        plt.show()
-
-    # return fit results so that we can write them to the H5 file
-    return (popt, pcov, popt_E, pcov_E)
+def plotsForAllDsets(h5file):
+    # function which creates histograms for all datasets
+    # in the /reconstruction/run_$#/chip_$# folders
+    pass
 
 def main(args):
 
@@ -333,6 +64,9 @@ def main(args):
                         default = None,
                         dest = "run_number",
                         help = "The run number to read from the file")
+    parser.add_argument('--all_runs',
+                        action = 'store_true',
+                        help = "Toggle this, if you want to run over all runs in the file")
     parser.add_argument('--chip',
                         default = None,
                         dest = "chip",
@@ -362,7 +96,8 @@ def main(args):
     outfolder = args_dict["outfolder"]
     chip = args_dict["chip"]
     fitting_only = args_dict["fitting_only"]
-    if chip is None or run_number is None:
+    all_runs = args_dict["all_runs"]
+    if chip is None or (run_number is None and all_runs == False):
         import sys
         sys.exit("Please provide a run and chip number!")
     
@@ -372,10 +107,12 @@ def main(args):
 
     group_name = recoDataChipBase(run_number) + str(chip)
     dset_name = args_dict["dset_name"]
-
-    data = readH5Data(h5file, group_name, [dset_name])
-    print("Read the following data {}".format(len(data[0])))
-
+                        
+    if all_runs == True:
+        group_name = recoBase()
+    data = readH5Data(h5file, group_name, chip, [dset_name])
+    #print("Read the following data {}".format(len(data[0])))
+        
 
     # in order to define cuts, we can do it the `ROOT` way and parse strings, which
     # are evaluated and then used. E.g.
@@ -387,18 +124,23 @@ def main(args):
 
 
     if dset_name != "FeSpectrum":
-        hist, binning = binData(data, cuts)
-        plotData(hist, binning, "fe_spectrum_{}".format(run_number), "Fe spectrum after cuts run {}".format(run_number), "Hits", "#")
+        #hist, binning = binData(data, cuts)
+        plotData(data, None,#binning,
+                 "{0}_{1}".format(dset_name, run_number),
+                 "{0} of run {1}".format(dset_name, run_number),
+                 dset_name,
+                 "# hits")
     else:
         fit_results = fitAndPlotFeSpectrum(data, cuts, outfolder, run_number, fitting_only)
 
-        # now finally write the fit results back to the H5 file
-        calib_factor = writeFitParametersH5(h5file, fit_results, group_name, dset_name)
+        if all_runs == False:
+            # now finally write the fit results back to the H5 file
+            calib_factor = writeFitParametersH5(h5file, fit_results, group_name, dset_name)
 
-        if fitting_only == True:
-            # now print the calibration factor again so that it can be read as the last line
-            # from a calling process
-            print("\n{}".format(calib_factor))
+            if fitting_only == True:
+                # now print the calibration factor again so that it can be read as the last line
+                # from a calling process
+                print("\n{}".format(calib_factor))
 
 
 if __name__=="__main__":

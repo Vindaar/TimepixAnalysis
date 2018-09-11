bdf303bc99c8efc44472c9bc8421b50ce6cc67fe
Vindaar <basti90@gmail.com>
2018-09-11 23:36:04 +0200

    also replace system `date` call by `CompileDate` magic

--- InGridDatabase/src/ingridDatabase.nim
+++ InGridDatabase/src/ingridDatabase.nim
@@ -22,10 +22,11 @@ import ingrid/calibration
 
 when defined(linux):
   const commitHash = staticExec("git rev-parse --short HEAD")
-  const currentDate = staticExec("date")
 else:
   const commitHash = ""
-  const currentDate = ""
+
+# get date using `CompileDate` magic
+const currentDate = CompileDate & " at " & CompileTime
 
 const docTmpl = """
 Version: $# built on: $#

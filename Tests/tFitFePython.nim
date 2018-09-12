3ecb98925ae9ffda5340728b45ac8bf4a0c9cfbe
Vindaar <basti90@gmail.com>
2018-09-12 17:50:12 +0200

    add simple test to test fitting Fe spectrum using Python

---
+++ Tests/tFitFePython.nim
@@ -0,0 +1,19 @@
+## test to check whether calling python from Nim works in
+## calibration.nim to fit the Fe spectrum
+
+import os
+import nimhdf5
+import strutils
+import ingrid / [calibration, tos_helpers]
+
+when isMainModule:
+  if paramCount() < 1:
+    quit("Please hand a h5 file!")
+
+  let h5file = paramStr(1)
+  var h5f = H5file(h5file, "rw")
+  h5f.visitFile()
+
+  for runStr, grp in runs(h5f):
+    let runNumber = runStr.parseInt
+    h5f.fitToFeSpectrum(runNumber, 3)

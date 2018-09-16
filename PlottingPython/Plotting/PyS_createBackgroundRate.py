1219a514289d19e9ccf1df7620ebb32cf6836488
Vindaar <basti90@gmail.com>
2018-09-16 22:35:06 +0200

    [background] allow plotting from 2 H5 likelihood cut files to cmp
    Produces a plot with 2 background rates now.

--- PlottingPython/Plotting/PyS_createBackgroundRate.py
+++ PlottingPython/Plotting/PyS_createBackgroundRate.py
@@ -11,6 +11,9 @@ import argparse
 from ingrid.ingrid_helper_functions import *
 import numpy as np
 
+color2017 = (99/255.0, 59/255.0, 171/255.0) #(220/255.0, 37/255.0, 102/255.0)#(147/255.0, 88/255.0, 254/255.0) #(99/255.0, 59/255.0, 171/255.0)
+color2014 = (253/255.0, 151/255.0, 31/255.0) #(147/255.0, 88/255.0, 254/255.0) #(220/255.0, 37/255.0, 102/255.0)
+
 def fancy_plotting():
     # set up some LaTeX plotting parameters
     # still need to change parameters
@@ -44,10 +47,74 @@ def readXrayData(h5file, chip):
     energy = readH5Data(h5file, group_name, chipNumber, ["energyFromCharge"])
     return energy
 
+def preparePlot(h5file, chip, logY):
+    year = ""
+    time_back = 0
+    shutter_open = 0
+    if "2014" in h5file:
+        year = "2014"
+        # Christoph 2014 / 15
+        time_back = 4000 * 3600
+        shutter_open = 0.97
+        color = color2014
+    elif "2017" in h5file:
+        # 2017
+        year = "2017"
+        time_back = 1123 * 3600
+        shutter_open = 0.88
+        color = color2017
+    else:
+        import sys
+        sys.exit("File needs to state if 2014 or 2017 data!")
+
+    if year == "2014":
+        chip = 0
+
+    energy = readXrayData(h5file, chip) # / 1000.0 division needed for E from P since in eV
+    print(np.shape(energy))
+    #print energy
+    hist, bin_edges = np.histogram(energy, bins=25, range=(0.2, 10))
+    hist_err = np.sqrt(hist)
+
+    # scale hist
+    if logY == False:
+        factor = 1e5
+    else:
+        factor = 1.0
+
+    area = (0.95 - 0.45)**2
+    bin_width = 0.392
+    scale = factor / (time_back * shutter_open * area * bin_width)
+    print("Scale is ", scale)
+    print("Hist is ", hist)
+    hist = hist * scale
+    print("Hist is now ", hist)
+    hist_err = hist_err * scale
+
+    bins = [(bin_edges[i+1] + bin_edges[i]) / 2.0 for i in range(len(bin_edges) - 1)]
+    plt.errorbar(bins, hist, yerr = hist_err, xerr = bin_width / 2.0,
+                 linestyle = '',
+                 marker = '.',
+                 markersize = 15,
+                 color = color,
+                 #color = (147/255.0, 88/255.0, 254/255.0),#'blue',
+                 #color = (186/255.0, 31/255.0, 86/255.0),#'blue',a
+                 #color = (220/255.0, 37/255.0, 102/255.0),
+                 #color = (99/255.0, 59/255.0, 171/255.0),
+                 #color = (253 / 255.0, 151 / 255.0, 31 / 255.0),
+                 linewidth = 2,
+                 label = year)
+
+    return year
+
 def main(args):
     parser = argparse.ArgumentParser(description = 'H5 Data plotter')
     parser.add_argument('file',
                         help = "The H5 file from which to read data")
+    parser.add_argument('--file2',
+                        default = None,
+                        dest = "file2",
+                        help = "A second H5 file to compare against")
     parser.add_argument('--region',
                         default = "gold",
                         dest = "region",
@@ -73,56 +140,14 @@ def main(args):
     region = args_dict["region"]
     chip = args_dict["chip"]
     logY = args_dict["log"]
+    h5file2 = args_dict["file2"]
     if fancy == True:
         fancy_plotting()
 
-    energy = readXrayData(h5file, chip) # / 1000.0 division needed for E from P since in eV
-    print(np.shape(energy))
-    #print energy
-    hist, bin_edges = np.histogram(energy, bins=25, range=(0.2, 10))
-    hist_err = np.sqrt(hist)
+    year = preparePlot(h5file, chip, logY)
+    if h5file2 is not None:
+        year = year + " and " + preparePlot(h5file2, chip, logY)
 
-    # scale hist
-    if logY == False:
-        factor = 1e5
-    else:
-        factor = 1.0
-    time_back = 0
-    shutter_open = 0
-    year = ""
-    if "2014" in h5file:
-        year = "2014"
-        # Christoph 2014 / 15
-        time_back = 4000 * 3600
-        shutter_open = 0.97
-    elif "2017" in h5file:
-        # 2017
-        year = "2017"
-        time_back = 1123 * 3600
-        shutter_open = 0.88
-    else:
-        import sys
-        sys.exit("File needs to state if 2014 or 2017 data!")
-    area = (0.95 - 0.45)**2
-    bin_width = 0.392
-    scale = factor / (time_back * shutter_open * area * bin_width)
-    print("Scale is ", scale)
-    print("Hist is ", hist)
-    hist = hist * scale
-    print("Hist is now ", hist)
-    hist_err = hist_err * scale
-
-    bins = [(bin_edges[i+1] + bin_edges[i]) / 2.0 for i in range(len(bin_edges) - 1)]
-    plt.errorbar(bins, hist, yerr = hist_err, xerr = bin_width / 2.0,
-                 linestyle = '',
-                 marker = '.',
-                 markersize = 15,
-                 #color = (147/255.0, 88/255.0, 254/255.0),#'blue',
-                 #color = (186/255.0, 31/255.0, 86/255.0),#'blue',a
-                 #color = (220/255.0, 37/255.0, 102/255.0),
-                 color = (99/255.0, 59/255.0, 171/255.0),
-                 #color = (253 / 255.0, 151 / 255.0, 31 / 255.0),
-                 linewidth = 2)
     if fancy == True:
         plt.xlabel('Energy / $\\si{\\keV}$')
         if logY == False:
@@ -138,6 +163,7 @@ def main(args):
     if logY == True:
         plt.semilogy()
     plt.grid()
+    plt.legend()
     plt.show()
 
 

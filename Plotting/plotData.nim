66fc7d699fd37fecec7d8d67b6a3ab9bd79c63b2
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2018-10-29 22:49:19 +0100

    [plotting] add occupancy plots to `plotData`

--- Plotting/plotData.nim
+++ Plotting/plotData.nim
@@ -11,6 +11,7 @@ import chroma
 import seqmath
 import options
 import mpfit
+import arraymancer
 import zero_functional
 import nimhdf5
 import dataset_helpers
@@ -57,8 +58,9 @@ Options:
                       The followoing backends are available:
                       Python / Matplotlib: {"python", "py", "matplotlib", "mpl"}
                       Nim / Plotly: {"nim", "plotly"}
-  --no_fadc           If set no FADC plots will be created
-  --no_ingrid         If set no InGrid plots will be created
+  --no_fadc           If set no FADC plots will be created.
+  --no_ingrid         If set no InGrid plots will be created.
+  --no_occupancy      If set no occupancy plots will be created.
   -h, --help          Show this help
   --version           Show the version number
 """
@@ -71,7 +73,7 @@ const
 
 type
   ConfigFlagKind = enum
-    cfNone, cfNoFadc, cfNoInGrid
+    cfNone, cfNoFadc, cfNoInGrid, cfNoOccupancy, cfNoPolya
 
   BackendKind = enum
     bNone, bMpl, bPlotly
@@ -90,6 +92,9 @@ type
 
   DataKind = enum
     dkInGrid, dkFadc
+
+  ShapeKind = enum
+    Rectangle, Square
 
 # global variable which stores the backend the user selected
 var BKind: BackendKind = bNone
@@ -113,9 +118,17 @@ proc plotHist*[T](traces: seq[Trace[T]], voltages: set[int16], chip = "") =
   let filename = &"out/scurves_{chip}.svg"
   p.saveImage(filename)
 
-proc getLayout(title: string, xlabel: string, ylabel: string): Layout =
+proc getLayout(title: string, xlabel: string, ylabel: string, shape = ShapeKind.Rectangle): Layout =
+  ## returns the plotly layout of the given shape
+  var width: int
+  case shape
+  of ShapeKind.Rectangle:
+    width = FigWidth.int
+  of ShapeKind.Square:
+    # for square take height as width
+    width = FigHeight.int
   result = Layout(title: title,
-                  width: FigWidth.int, height: FigHeight.int,
+                  width: width, height: FigHeight.int,
                   xaxis: Axis(title: xlabel),
                   yaxis: Axis(title: ylabel),
                   autosize: false)
@@ -214,12 +227,12 @@ proc plotHistsFeSpec(h5f: var H5FileObj,
       let idxFadc = (toSeq(0 .. evNumFadc.high)) --> filter(evNumFadc[it] in inGridSet)
       let dset = h5f[(group.name / dsetName).dset_str]
       case dset.dtypeAnyKind
-      of akUint16: # minvals
+      of akUint16: # fallTime
         var ldata = dset[uint16]
         ldata = idxFadc --> map(ldata[it])
         let title = &"Dataset: {dsetName} for fadc in range: {r[2]}"
         plotHist(ldata, title, dsetName)
-      of akFloat64: # fallTime
+      of akFloat64: # minVals
         var ldata = dset[float]
         ldata = idxFadc --> map(ldata[it])
         let title = &"Dataset: {dsetName} for fadc in range: {r[2]}"
@@ -227,19 +240,20 @@ proc plotHistsFeSpec(h5f: var H5FileObj,
       else:
         echo "Unsupported type ", dset.dtypeAnyKind
         discard
-
+    else:
+      echo "No data kind selected: ", dKind
 
 proc histograms(h5f: var H5FileObj, flags: set[ConfigFlagKind]) =
   const dsets = ["length", "width", "skewnessLongitudinal", "skewnessTransverse",
                  "kurtosisLongitudinal", "kurtosisTransverse", "rotationAngle",
                  "eccentricity", "fractionInTransverseRms", "lengthDivRmsTrans"]
-  const fadcDsets = ["minvals", "fallTime"]                     
+  const fadcDsets = ["minvals", "fallTime"]
   # TODO: perform cut on photo peak and escape peak, done by getting the energy
   # and performing a cut around peak +- 300 eV maybe
   # NOTE: need photo peak and escape peak plenty of times here. Just write wrapper
   # which takes energy and performs cuts, *then* creates plots
-  
-  
+
+
   for runNumber, group in runs(h5f):
     var group = h5f[group.grp_str]
     if cfNoIngrid notin flags:
@@ -253,19 +267,97 @@ proc histograms(h5f: var H5FileObj, flags: set[ConfigFlagKind]) =
       for dsetName in fadcDsets:
         plotHistsFeSpec(h5f, fadcGrp, dsetName, dkFadc)
 
+proc calcOccupancy(x, y: seq[seq[uint8]]): Tensor[int] =
+  ## calculates the occupancy of the given x and y datasets
+  result = newTensor[int]([256, 256])
+  # iterate over events
+  for i in 0 .. x.high:
+    let
+      xEv = x[i]
+      yEv = y[i]
+    for j in 0 .. xEv.high:
+      result[xEv[j].int, yEv[j].int] += 1
+
+proc plotHist2D[T](data: Tensor[T], title, descr: string) =
+  ## creates a 2D histogram plot (basically an image) of the given 2D
+  ## Tensor
+  doAssert data.rank == 2
+  case BKind
+  of bPlotly:
+    let lyout = getLayout(title, "pixel x", "pixel y", ShapeKind.Square)
+    let tr = Trace[T](`type`: PlotType.HeatMap,
+                      colormap: ColorMap.Viridis,
+                      zs: data.toRawSeq.reshape2D([256, 256]))
+    let p = Plot[T](layout: lyout, traces: @[tr])
+    p.show()
+  of bMpl:
+    discard
+  else:
+    discard
+
+proc plotOccupancies(h5f: var H5FileObj,
+                     group: H5Group,
+                     chipNum: int,
+                     runNumber: string) =
+  ## creates 3 occupancy plots for the given run & chip for different ranges
+  ## - 1 full range
+  ## - 1 clamped to .75 percentile
+  ## - 1 clamped to 10 hits
+  const
+    quantile = 95
+    clamp3 = 10
+  # get x and y datasets, stack and get occupancies
+  let vlenDtype = special_type(uint8)
+  let
+    xGroup = h5f[(group.name / "x").dset_str]
+    yGroup = h5f[(group.name / "y").dset_str]
+    x = xGroup[vlenDtype, uint8]
+    y = yGroup[vlenDtype, uint8]
+  let occ = calcOccupancy(x, y)
+  # calculate 3 clamp values based on occ
+  let quant = occ.toRawSeq.percentile(quantile)
+  let clamps = [int.high, quant.int, clamp3]
+  for cl in clamps:
+    var title = &"Occupancy of chip {chipNum} for run {runNumber}"
+    if cl == int.high:
+      title = title & " in full range"
+      plotHist2D(occ, title, "Occupancy")
+    else:
+      let occClamped = occ.clamp(0, cl)
+      title = title & &" clamped to: {cl}"
+      plotHist2D(occClamped, title, "Occupancy")
+
+proc occupancies(h5f: var H5FileObj, flags: set[ConfigFlagKind]) =
+  ## creates occupancy plots for the given HDF5 file, iterating over
+  ## all runs and chips
+  for runNumber, grpName in runs(h5f):
+    var group = h5f[grpName.grp_str]
+    # now build occupancy
+    for chipNum, chpgrp in chips(group):
+      plotOccupancies(h5f, chpgrp, chipNum, runNumber)
+
+proc polya(h5f: var H5FileObj, flags: set[ConfigFlagKind]) =
+  ## creates the plots for the polya distribution of the datasets
+  # for now try to read polya datasets?!
+  discard
+
 proc createCalibrationPlots(h5file: string, bKind: BackendKind,
                             flags: set[ConfigFlagKind]) =
   ## creates QA plots for calibration runs
   var h5f = H5file(h5file, "r")
   const length = "length"
-  # occupancies(h5f) # plus center only
-  # polya(h5f)
+  if cfNoOccupancy notin flags:
+    occupancies(h5f, flags) # plus center only
+  if cfNoPolya notin flags:
+    polya(h5f, flags)
   # feSpectrum(h5f)
   # centerFePerTime(h5f)
   # energyCalib(h5f) # ????
   histograms(h5f, flags) # including fadc
   # likelihoodHistograms(h5f) # need to cut on photo peak and esc peak
   # neighborPixels(h5f)
+
+  discard h5f.close()
 
 
 proc createBackgroundPlots(bKind: BackendKind, flags: set[ConfigFlagKind]) =

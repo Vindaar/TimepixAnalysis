0c98f1103b76ce12a2b810a0ce0448b88b53261a
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2018-11-08 15:47:30 +0100

    [Python] fix import name of `procsForPython`

--- InGrid-Python/ingrid/fit_fe_spectrum.py
+++ InGrid-Python/ingrid/fit_fe_spectrum.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 mpl.use("TKagg")
 import sys
 sys.path.append("./")
-import ingrid.procsForPython
+import ingrid.procsForPython as procsForPython
 
 # this whole fit is probably going to be pretty slow.
 # convert to Nim functions, which we compile to lib and call

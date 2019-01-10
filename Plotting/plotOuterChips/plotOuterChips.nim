d341afd6e8d60dc5334180ef9938ea0394bbd43f
Vindaar <basti90@gmail.com>
2019-01-10 19:45:07 +0100

    [plotting] add script to create OuterChips hist plot
    Serves as an example how to use `karaPlot` as a library to create
    plots.
    
    The script creates the histogram for the number of hits of events on
    the outer chips, in case there was an event on the center chip which
    is "blob" like, meaning passing simple cuts on eccentricity and
    rmsTransverse.
    
    Background and calibration H5 files are combined and the histograms overlaid.

---
+++ Plotting/plotOuterChips/plotOuterChips.nim
@@ -0,0 +1,73 @@
+import .. / karaPlot / plotData
+import docopt, strutils
+import nimhdf5, plotly, chroma
+import ingrid / ingrid_types
+
+when defined(linux):
+  const commitHash = staticExec("git rev-parse --short HEAD")
+else:
+  const commitHash = ""
+
+# get date using `CompileDate` magic
+const currentDate = CompileDate & " at " & CompileTime
+
+const docTmpl = """
+Version: $# built on: $#
+A tool to plot a histogram of events on outer chips
+
+Usage:
+  plotOuterChips <H5file> <H5file2> [options]
+
+Options:
+  -h, --help             Show this help
+  --version              Show the version number
+"""
+const doc = docTmpl % [commitHash, currentDate]
+
+proc makeOuterChipPlot(fname: string, runType: RunTypeKind): PlotV =
+  var h5f = H5file(fname, "r")
+  let fInfo = getFileInfo(h5f)
+  let pds = createOuterChipHistograms(h5f,
+                                      runType = runType,
+                                      fileInfo = fInfo)
+  let (name, plt) = createPlot(h5f, fInfo, pd)
+  result = plt
+
+proc main =
+
+  # somewhat ugly hack
+  plotData.BKind = bPlotly
+
+  let args = docopt(doc)
+  let backFname = $args["<H5file>"]
+  let calibFname = $args["<H5file2>"]
+
+  let pltBack = makeOuterChipPlot(backFname, rtBackground)
+  let pltCalib = makeOuterChipPlot(calibFname, rtCalibration)
+
+  let gridColor = color(0.8, 0.8, 0.8, 0.8)
+
+  var plt = pltBack.plPlot
+  plt = plt.addTrace(pltCalib.plPlot.traces[0])
+    .title("# pixels on outer chips for blob on center")
+    .xlabel("Outer chips # pixels hit")
+    .gridColor(gridColor)
+    .markerColor(@[color(1.0, 0.0, 102.0 / 256.0)], idx = 0)
+    .markerColor(@[color(0.0, 153.0 / 256.0, 204 / 256.0)], idx = 1)
+
+  plt.traces[0].opacity = 1.0
+  plt.traces[1].opacity = 0.5
+  plt.layout.barMode = BarMode.Overlay
+
+  plt.traces[0].histNorm = HistNorm.None
+  plt.traces[1].histNorm = HistNorm.None
+  plt.show()
+
+  plt.traces[0].histNorm = HistNorm.ProbabilityDensity
+  plt.traces[1].histNorm = HistNorm.ProbabilityDensity
+  plt.layout.yaxis.title = "Probability density"
+
+  plt.show()
+
+when isMainModule:
+  main()

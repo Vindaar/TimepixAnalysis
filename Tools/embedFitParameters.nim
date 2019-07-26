d041b56ceffb7c4afafc44027c2619df204eb700
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2019-07-26 14:38:36 +0200

    [tools] remove dead code in embedFitParameters

--- Tools/embedFitParameters.nim
+++ Tools/embedFitParameters.nim
@@ -117,17 +117,3 @@ proc main =
 
 when isMainModule:
   main()
-
-#buildSvgFile("examples/text1.svg"):
-#  let s = "b"
-#  svg(width=100, height=100, xmlns="http://www.w3.org/2000/svg", version="1.1", baseProfile="full"):
-#    text(x=50,
-#         y=50,
-#         `text-anchor`="middle",
-#         `dominant-baseline`="central",
-#         style="font-family: 'Open Sans'"):
-#         #transform="rotate(-90 50 50)"):
-#      tspan(x=50, dy="1.2em"):
-#        t: "Hello World"
-#      tspan(x=50, dy="1.2em"):
-#        t: "Ok!"

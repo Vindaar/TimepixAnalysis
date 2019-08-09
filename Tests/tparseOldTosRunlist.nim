1fe6190bd3d121ef729435696805570e44be316d
Sebastian Schmidt <s.schmidt@physik.uni-bonn.de>
2019-08-09 16:03:32 +0200

    [tests] use `unittest` for `tparseOldTosRunList`

--- Tests/tparseOldTosRunlist.nim
+++ Tests/tparseOldTosRunlist.nim
@@ -1,17 +1,22 @@
 import ingrid / [tos_helpers, ingrid_types]
-import strformat
+import strformat, os, strutils
+import unittest
 
-when isMainModule:
-  const path = "/mnt/Daten/CAST/2014_15/D03-W0063/Runlist-CAST-D03-W0063.csv"
+suite "Parse old TOS CSV file (2014/15)":
+  test "Correct number of runs found":
+    const path = currentSourcePath().parentDir / "../resources/Runlist-CAST-D03-W0063.csv"
 
-  let calib = parseOldTosRunlist(path, rtCalibration)
-  let back = parseOldTosRunlist(path, rtBackground)
-  let xray = parseOldTosRunlist(path, rtXrayFinger)
+    let calib = parseOldTosRunlist(path, rtCalibration)
+    let back = parseOldTosRunlist(path, rtBackground)
+    let xray = parseOldTosRunlist(path, rtXrayFinger)
 
-  echo &"Found {calib.card} calibration runs"
-  echo &"Found {back.card} background runs"
-  echo &"Found {xray.card} X-ray finger runs"
+    echo &"Found {calib.card} calibration runs"
+    echo &"Found {back.card} background runs"
+    echo &"Found {xray.card} X-ray finger runs"
+    check calib.card == 189
+    check back.card == 224
+    check xray.card == 4
 
-  echo "Calibration runs: ", calib
-  echo "Background runs: ", back
-  echo "Xray-Finger runs: ", xray
+    echo "Calibration runs: ", calib
+    echo "Background runs: ", back
+    echo "Xray-Finger runs: ", xray

7deeafeeaf0e8f006bb9d2e102a40f1cf592b69d
Vindaar <basti90@gmail.com>
2019-12-09 18:28:52 +0100

    [MarlinInjection] perform injection on all calibration runs

--- Tools/CompareMarlinTpa/UseMarlinRotAngle/runHackedAnalysis.nim
+++ Tools/CompareMarlinTpa/UseMarlinRotAngle/runHackedAnalysis.nim
@@ -8,13 +8,17 @@ if fileExists($recoPath / "config.nims"):
   undoBackup = true
 shell:
   cp runHackedAnalysis.nims ($recoPath)/config.nims
-shell:
-  nim c "-f --threads:on -d:danger -d:activateHijack" ($recoPath)/reconstruction.nim
+let res = shellVerbose:
+  nim c "-f -d:danger -d:release --threads:on -d:activateHijack -d:hijackCalibration" ($recoPath)/reconstruction.nim
+if res[1] != 0:
+  quit("Compilation failed")
 if undoBackup:
   shell:
     cp ($recoPath)/config.nims_backup ($recoPath)/config.nims
 else:
   shell:
     rm ($recoPath)/config.nims
+let outfile = "/mnt/1TB/CAST/CalibrationRuns_2014_MarlinHijackedCluster.h5"
 shell:
-  "../../../Analysis/ingrid/reconstruction ../../../Tests/run_245_2014.h5" "--out" testfile.h5
+  #"../../../Analysis/ingrid/reconstruction ../../../Tests/run_245_2014.h5" "--out" testfile.h5
+  "../../../Analysis/ingrid/reconstruction /mnt/1TB/CAST/CalibrationRuns_2014_Raw.h5" "--out" ($outfile) "--runNumber 256"

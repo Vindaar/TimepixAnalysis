9cad65728a8d2a4868ff444aa9ea357473c7599e
Vindaar <basti90@gmail.com>
2019-12-16 18:57:48 +0100

    [M/TPA] make runHackedAnalysis a tool to run whole ana chain

--- Tools/CompareMarlinTpa/UseMarlinRotAngle/runHackedAnalysis.nim
+++ Tools/CompareMarlinTpa/UseMarlinRotAngle/runHackedAnalysis.nim
@@ -1,40 +1,113 @@
 import shell, strutils, os
 
-const isBackground = false
-when isBackground:
-  let prefix = "DataRuns"
-else:
-  let prefix = "CalibrationRuns"
+proc checkRes[T](res: T, msg = "Compilation failed") =
+  when T is tuple:
+    if res[1] != 0:
+      quit(msg)
+  else:
+    if res != 0:
+      quit(msg)
 
+proc copyConfigs(recoPath: string): bool =
+  if fileExists($recoPath / "config.nims"):
+    let res = shellVerbose:
+      cp ($recoPath)/config.nims ($recoPath)/config.nims_backup
+    checkRes res, "backing up config.nims file failed!"
+    result = true
+  let res = shellVerbose:
+    cp runHackedAnalysis.nims ($recoPath)/config.nims
+  checkRes res, "copying confing.nims failed"
 
-let recoPath = "../../../Analysis/ingrid/"
-var undoBackup = false
-if fileExists($recoPath / "config.nims"):
-  shell:
-    cp ($recoPath)/config.nims ($recoPath)/config.nims_backup
-  undoBackup = true
-shell:
-  cp runHackedAnalysis.nims ($recoPath)/config.nims
-when isBackground:
+proc doUndoBackup(recoPath: string, undo: bool) =
+  if undo:
+    let res = shellVerbose:
+      cp ($recoPath)/config.nims_backup ($recoPath)/config.nims
+    checkRes res, "reverting config.nims from backup failed"
+  else:
+    let res = shellVerbose:
+      rm ($recoPath)/config.nims
+    checkRes res, "removing config.nims failed"
+
+const commonArgs = "-f -d:danger -d:release --threads:on -d:activateHijack"
+proc compileBackground(recoPath: string): int =
   let res = shellVerbose:
-    nim c "-f -d:danger -d:release --threads:on -d:activateHijack -d:hijackBackground" ($recoPath)/reconstruction.nim
-else:
+    nim c ($commonArgs) "-d:hijackBackground" ($recoPath)/reconstruction.nim
+  result = res[1]
+
+proc compileCalibration(recoPath: string): int =
   let res = shellVerbose:
-    nim c "-f -d:danger -d:release --threads:on -d:activateHijack -d:hijackCalibration" ($recoPath)/reconstruction.nim
-if res[1] != 0:
-  quit("Compilation failed")
-if undoBackup:
-  shell:
-    cp ($recoPath)/config.nims_backup ($recoPath)/config.nims
-else:
-  shell:
-    rm ($recoPath)/config.nims
-let outfile = &"/mnt/1TB/CAST/{prefix}_2014_MarlinHijackedCluster.h5"
-let infile = &"/mnt/1TB/CAST/{prefix}_2014_Raw.h5"
-echo "INFILE ", infile
-echo "OUTFILE ", outfile
-shell:
-  #"../../../Analysis/ingrid/reconstruction ../../../Tests/run_245_2014.h5" "--out" testfile.h5
-  "../../../Analysis/ingrid/reconstruction" ($infile) "--out" ($outfile)
-  #"../../../Analysis/ingrid/reconstruction /mnt/1TB/CAST/CalibrationRuns_2014_Raw.h5" "--out" ($outfile) "--runNumber 300"
-  #"../../../Analysis/ingrid/reconstruction" ($outfile) "--runNumber 300 --only_fe_spec"
+    nim c ($commonArgs) "-d:hijackCalibration" ($recoPath)/reconstruction.nim
+  result = res[1]
+
+proc getFile(prefix: string, raw = false): string =
+  if raw:
+    result = &"/mnt/1TB/CAST/{prefix}_2014_Raw.h5"
+  else:
+    result = &"/mnt/1TB/CAST/{prefix}_2014_MarlinHijackedCluster.h5"
+
+proc runRawToReco(prefix: string) =
+  let infile = getFile(prefix, raw = true)
+  let outfile = getFile(prefix, raw = false)
+  let res = shellVerbose:
+    "../../../Analysis/ingrid/reconstruction" ($infile) "--out" ($outfile)
+  checkRes res
+
+proc runRecoFlags() =
+  const recoOptions = ["--only_charge", "--only_gas_gain",
+                       #"--only_gain_fit",
+                       "--only_energy_from_e"]
+  const filePrefixes = ["DataRuns"] # ["CalibrationRuns", "DataRuns"]
+  for f in filePrefixes:
+    let infile = getFile(f, raw = false)
+    for opt in recoOptions:
+      let res = shellVerbose:
+        "../../../Analysis/ingrid/reconstruction" ($infile) ($opt)
+      checkRes res, "running reconstruction on " & $infile & " with opt " & $opt & " failed!"
+
+proc runLogReader() =
+  let file = "/mnt/1TB/CAST/DataRuns_2014_MarlinHijackedCluster.h5"
+  let logFolder = "../../../resources/LogFiles/tracking-logs"
+  template run(args: string): untyped =
+    block:
+      let res = shellVerbose:
+        "../../../LogReader/cast_log_reader" ($logFolder) "--h5out" ($file) ($args)
+      checkRes res, "running log reader failed with args " & $args
+  # first potentially delete log files
+  run("--delete")
+  run("")
+
+proc runLikelihood() =
+  let lPath = "../../../Analysis/ingrid/likelihood"
+  let file = "/mnt/1TB/CAST/DataRuns_2014_MarlinHijackedCluster.h5"
+  let outname = "/mnt/1TB/CAST/LHood_2014_MarlinHijackedCluster.h5"
+  let res = shellVerbose:
+    ($lPath) ($file) "--h5out" ($outname)
+
+proc runFull() =
+  runRawToReco("CalibrationRuns")
+  runRawToReco("DataRuns")
+
+when isMainModule:
+  let isBackground = false
+  let recoPath = "../../../Analysis/ingrid/"
+  let toUndo = copyConfigs(recoPath)
+  var res: int
+  if isBackground:
+    res = compileBackground(recoPath)
+  else:
+    res = compileCalibration(recoPath)
+  checkRes res
+
+  let prefix = block:
+    if isBackground:
+      "DataRuns"
+    else:
+      "CalibrationRuns"
+  #runRawToReco(prefix)
+  #runRecoFlags()
+  #runLogReader()
+  runLikelihood()
+
+  var undoBackup = false
+  if undoBackup:
+    doUndoBackup(recoPath, toUndo)

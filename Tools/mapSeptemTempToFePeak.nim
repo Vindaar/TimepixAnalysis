f1f74b438ca305cb9f8e9a2989265395c74eab4a
Vindaar <basti90@gmail.com>
2020-09-21 00:14:30 +0200

    add a script to normalize Fe peak positions by temp and compare
    This should at least to an extent put the though to rest that there is
    a high correlation between the peak position and the temperature of
    the detector.

---
+++ Tools/mapSeptemTempToFePeak.nim
@@ -0,0 +1,109 @@
+import mclimit
+import os except FileInfo
+import ggplotnim, seqmath, sequtils, tables, options, times
+import nimhdf5, cligen
+
+import ingrid / [tos_helpers, ingrid_types]
+
+
+type
+  FeFileKind = enum
+    fePixel, feCharge
+
+const TempFile = "../resources/cast_2017_2018_temperatures.csv"
+const OrgFormat = "'<'yyyy-MM-dd ddd H:mm'>'"
+var dfTemp = toDf(readCsv(TempFile))
+  .filter(f{c"Temp / °" != "-"})
+dfTemp["Timestamp"] = dfTemp["Date"].toTensor(string).map_inline(parseTime(x, OrgFormat, utc()).toUnix)
+
+proc readFePeaks(files: seq[string], feKind: FeFileKind = fePixel): DataFrame =
+  const kalphaPix = 10
+  const kalphaCharge = 4
+  const parPrefix = "p"
+  const dateStr = "yyyy-MM-dd'.'HH:mm:ss" # example: 2017-12-04.13:39:45
+  var dset: string
+  var kalphaIdx: int
+  case feKind
+  of fePixel:
+    kalphaIdx = kalphaPix
+    dset = "FeSpectrum"
+  of feCharge:
+    kalphaIdx = kalphaCharge
+    dset = "FeSpectrumCharge"
+
+  var h5files = files.mapIt(H5File(it, "r"))
+  var fileInfos = newSeq[FileInfo]()
+  for h5f in mitems(h5files):
+    let fi = h5f.getFileInfo()
+    fileInfos.add fi
+  var
+    peakSeq = newSeq[float]()
+    dateSeq = newSeq[float]()
+  for (h5f, fi) in zip(h5files, fileInfos):
+    for r in fi.runs:
+      let group = h5f[(recoBase() & $r).grp_str]
+      let chpGrpName = group.name / "chip_3"
+      peakSeq.add h5f[(chpGrpName / dset).dset_str].attrs[
+        parPrefix & $kalphaIdx, float
+      ]
+      dateSeq.add parseTime(group.attrs["dateTime", string],
+                            dateStr,
+                            utc()).toUnix.float
+  result = seqsToDf({ "PeakPos" : peakSeq,
+                      "Timestamp" : dateSeq })
+    .arrange("Timestamp", SortOrder.Ascending)
+
+proc findTemp(d: int, dates: seq[int], temps: Tensor[float]): float =
+  let idx = dates.lowerBound(d)
+  let idxMin = max(0, idx - 1)
+  if idx == idxMin:
+    result = temps[idx]
+  else:
+    let
+      idxMoreDiff = dates[idx] - d
+      idxLessDiff = dates[idxMin] - d
+      # interpolate temp between the two temps corresponding both idx
+      t0 = temps[idxMin]
+      t1 = temps[idx]
+      totDiff = dates[idx] - dates[idxMin]
+    echo "tot diff ", totDiff, "   ", fromUnix(dates[idx]), "    ", fromUnix(dates[idxMin]), "   ", fromUnix(d)
+    result = (t0 * (abs(totDiff - idxLessDiff).float) +
+              t1 * abs(totDiff - idxMoreDiff).float) /
+      totDiff.float
+
+proc main(calibFiles: seq[string]) =
+
+  var dfPeaks = readFePeaks(calibFiles)
+  ggplot(dfPeaks, aes("Timestamp", "PeakPos")) +
+    geom_point() +
+    ggsave("/tmp/time_vs_peak_pos.pdf")
+
+  let runs = dfTemp["Run number"].toTensor(int)
+  let temps = dfTemp["Temp / °"].toTensor(float)
+  let dates = dfTemp["Timestamp"].toTensor(int).toRawSeq
+  let feDates = dfPeaks["Timestamp"].toTensor(int)
+  let peaks = dfPeaks["PeakPos"].toTensor(float)
+  var peakNorm = newSeq[float](peaks.size)
+
+  var idx = 0
+  for date in feDates:
+    let temp = findTemp(date, dates, temps)
+    peakNorm[idx] = peaks[idx] / (temp + 273.15)
+    echo "Temp ", temp, " peak ", peaks[idx], " norm ", peakNorm[idx]
+    inc idx
+  var df = seqsToDf({ "PeakNorm" : peakNorm, "Timestamp" : feDates })
+  ggplot(df, aes(Timestamp, PeakNorm)) +
+    geom_point() +
+    ggsave("/tmp/time_vs_peak_norm_by_temp.pdf")
+
+  # finally combine the twe into one plot
+  dfPeaks = dfPeaks.mutate(f{`PeakPos` ~ `PeakPos` / max(`PeakPos`)})
+  df = df.mutate(f{`PeakNorm` ~ `PeakNorm` / max(`PeakNorm`)})
+  df = innerJoin(df, dfPeaks, by = "Timestamp").gather(["PeakNorm", "PeakPos"], "Type", "peak")
+  ggplot(df, aes(Timestamp, peak, color = "Type")) +
+    geom_point() +
+    ggsave("/tmp/time_vs_peak_temp_normed_comparison.pdf")
+
+
+when isMainModule:
+  dispatch(main, echoResult = false, noAutoEcho = true)

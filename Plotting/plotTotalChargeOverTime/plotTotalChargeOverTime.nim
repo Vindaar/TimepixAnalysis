8496732d653a0152c69d1c46024d03cbd885bc92
Vindaar <basti90@gmail.com>
2020-11-09 15:25:36 +0100

    [plots] create facet wrap of plots in different run periods
    A run period here is defined by being separated by more than 5 days in time.

--- Plotting/plotTotalChargeOverTime/plotTotalChargeOverTime.nim
+++ Plotting/plotTotalChargeOverTime/plotTotalChargeOverTime.nim
@@ -1,5 +1,5 @@
 import nimhdf5, ggplotnim, os, strformat
-import ingrid / tos_helpers
+import ingrid / tos_helpers, times
 
 import cligen
 
@@ -50,6 +50,10 @@ proc caclulateMeanDf(df: DataFrame, interval: float): DataFrame =
   # I fear. So just do it manually...
   let tstamps = df["timestamp"].toTensor(float)
   let totCh = df["totalCharge"].toTensor(float)
+  proc toPeriod(t: float): string =
+    ## convert t to unix timestamp and then format date
+    result = fromUnix(t.int).format("dd/MM/YYYY")
+
   var
     curSum = 0.0
     curNum = 0
@@ -59,36 +63,59 @@ proc caclulateMeanDf(df: DataFrame, interval: float): DataFrame =
     sums = newSeq[float]()
     means = newSeq[float]()
     tmeanStamps = newSeq[float]()
+    runPeriods = newSeq[string]()
+    lastPeriod = toPeriod tStart
+  const splitPeriod = 86400 * 5 # 5 days?
   for i in 0 ..< tstamps.size:
-    if (tstamps[i] - tStart) >= (interval * 60.0):
+    let inInterval = (tstamps[i] - tStart) < (interval * 60.0)
+    if not inInterval:
       # run over bin range
       sums.add curSum
       means.add (curSum / curNum.float)
-      tmeanStamps.add (tstamps[i] + tStart) / 2.0
+      let curTstamp = (tstamps[i] + tStart) / 2.0
+      if curTstamp - tmeanStamps[^1] >= splitPeriod:
+        lastPeriod = toPeriod curTstamp
+      tmeanStamps.add curTstamp
       curSum = 0.0
       curNum = 0
       tStart = tstamps[i]
+      runPeriods.add lastPeriod
     curSum += totCh[i]
     inc curNum
   result = seqsToDf({"timestamp" : tmeanStamps, "sumCharge" : sums,
-                      "meanCharge" : means })
+                      "meanCharge" : means, "runPeriods" : runPeriods })
+  let filterPeriods = result.group_by("runPeriods").count("runPeriods")
+    .filter(f{`n` == 1})["runPeriods"].toTensor(string).toRawSeq
+  result = result.filter(f{string -> bool: `runPeriods` notin filterPeriods})
+  echo "Number of run periods with more than 1 entry: ", result["runPeriods"].unique
 
-proc plotDf(df: DataFrame, interval: float, titleSuff: string) =
+proc formatTime(v: float): string =
+  result = v.int.fromUnix.format("dd/MM/YYYY")
+
+proc plotDf(df: DataFrame, interval: float, titleSuff: string,
+            useLog = true) =
   const outpath = "out"
   createDir(outpath)
   let nameSuff = if titleSuff == "all data": "all" else: "filtered"
-  ggplot(df, aes("timestamp", "sumCharge")) +
+  var pltSum = ggplot(df, aes("timestamp", "sumCharge")) +
+    facet_wrap("runPeriods", scales = "free") +
     geom_point() +
-    scale_y_log10() +
-    ggtitle(&"Sum of total charge within {interval:.1f} min, {titleSuff}") +
-    ggsave(&"{outpath}/background_sum_charge_binned_{interval:.1f}_min_{nameSuff}.pdf",
-            width = 800, height = 480)
-  ggplot(df, aes("timestamp", "meanCharge")) +
+    scale_x_continuous(labels = formatTime) +
+    xlab(rotate = -45, alignTo = "right") +
+    ggtitle(&"Sum of total charge within {interval:.1f} min, {titleSuff}")
+  var pltMean = ggplot(df, aes("timestamp", "meanCharge")) +
+    facet_wrap("runPeriods", scales = "free") +
+    scale_x_continuous(labels = formatTime) +
+    xlab(rotate = -45, alignTo = "right") +
     geom_point() +
-    scale_y_log10() +
-    ggtitle(&"Mean of total charge within {interval:.1f} min, {titleSuff}") +
-    ggsave(&"{outpath}/background_mean_charge_binned_{interval:.1f}_min_{nameSuff}.pdf",
-            width = 800, height = 480)
+    ggtitle(&"Mean of total charge within {interval:.1f} min, {titleSuff}")
+  if useLog:
+    pltSum = pltSum + scale_y_log10()
+    pltMean = pltSum + scale_y_log10()
+  pltSum + ggsave(&"{outpath}/background_sum_charge_binned_{interval:.1f}_min_{nameSuff}.pdf",
+                   width = 1920, height = 1080)
+  pltMean + ggsave(&"{outpath}/background_mean_charge_binned_{interval:.1f}_min_{nameSuff}.pdf",
+                    width = 1920, height = 1080)
 
   ggplot(df, aes("meanCharge")) +
     geom_histogram() +
@@ -108,8 +135,12 @@ proc main(files: seq[string], interval, cutoffCharge, cutoffHits: float) =
                            f{float -> bool: `hits` < cutoffHits})
     .caclulateMeanDf(interval)
 
+  echo dfFilter
+  echo dfAll
   plotDf(dfAll, interval, "all data")
-  plotDf(dfFilter, interval, &"charge > {cutoffCharge}, hits < {cutoffHits:.0f} filtered out")
+  plotDf(dfFilter, interval,
+         &"charge > {cutoffCharge}, hits < {cutoffHits:.0f} filtered out",
+         useLog = false)
 
 
 when isMainModule:

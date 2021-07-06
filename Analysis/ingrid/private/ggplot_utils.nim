ca792c5eccc02065a7284ba1604ccda03f09c041
Vindaar <basti90@gmail.com>
2021-07-06 09:46:45 +0200

    [utils] add (commented out) writeCsv option for septem events

--- Analysis/ingrid/private/ggplot_utils.nim
+++ Analysis/ingrid/private/ggplot_utils.nim
@@ -252,6 +252,7 @@ proc plotSeptemEvent*(evData: PixelsInt, run, eventNumber: int,
     yCol[i] = ev.y
     chCol[i] = ev.ch
   let df = seqsToDf({"x" : xCol, "y" : yCol, "charge" : chCol})
+  #writeCsv(df, &"/tmp/septemEvent_run_{run}_event_{eventNumber}.csv")
   ggplot(df, aes(x, y, color = charge)) +
     geom_point(size = some(1.0)) +
     xlim(0, 768) + ylim(0, 768) + scale_x_continuous() + scale_y_continuous() +

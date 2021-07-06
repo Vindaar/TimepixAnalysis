4d2a8bac0e3c14b8b0e481f9d74cf0a0f3be3abc
Vindaar <basti90@gmail.com>
2021-07-06 16:00:45 +0200

    [hough] improve hough transformation playground, add intersect & suffix

--- Tools/houghTrafoPlayground/houghTrafoPlayground.nim
+++ Tools/houghTrafoPlayground/houghTrafoPlayground.nim
@@ -1,5 +1,5 @@
 import ggplotnim
-import cligen, sets, hashes, tables
+import cligen, sets, hashes, tables, strformat
 
 #proc hashtest(num: int): DataFrame =
 #  var ids = newSeqOfCap[string](num * num)
@@ -23,13 +23,14 @@ import cligen, sets, hashes, tables
 #  echo "set length ", s.toHashSet().card
 #  echo "ids to hash set ", ids.toHashSet().card
 
-proc houghTrafo[T: seq | Tensor](x, y: T): DataFrame =
+proc houghTrafo[T: seq | Tensor](x, y: T, suffix: int): DataFrame =
   ## computes a hough transformation of `x` and `y`
   doAssert x.len == y.len
   var xs = newSeqOfCap[int](x.len * x.len)
   var ys = newSeqOfCap[int](x.len * x.len)
   var ids = newSeqOfCap[string](x.len * x.len)
   var slopes = newSeqOfCap[float](x.len * x.len)
+  var intersects = newSeqOfCap[float](x.len * x.len)
   echo x
   for i in 0 ..< x.len:
     #echo "at ", i
@@ -41,26 +42,34 @@ proc houghTrafo[T: seq | Tensor](x, y: T): DataFrame =
         ys.add y[i]
         ids.add $i & "/" & $j
         ids.add $i & "/" & $j
-        #echo "Ys ", ys[^1], "  ", ys[^2]
-        #echo "Xs ", xs[^1], "  ", xs[^2]
-        slopes.add (ys[^1] - ys[^2]).float / (xs[^1] - xs[^2]).float
+        if xs[^1] - xs[^2] > 0:
+          let slope = (ys[^1] - ys[^2]).float / (xs[^1] - xs[^2]).float
+          slopes.add slope
+          doAssert abs((y[j].float - slope * x[j].float) - (y[i].float - slope * x[i].float)) < 1e-4
+          intersects.add (y[j].float - slope * x[j].float)
   result = toDf(xs, ys, ids)
-  let df = toDf(slopes).filter(f{`slopes` <= 3.0 and `slopes` >= -3.0})
+  let df = toDf(slopes, intersects).filter(f{`slopes` <= 3.0 and `slopes` >= -3.0})
   ggplot(df, aes("slopes")) +
     geom_histogram(bins = 100) +
-    ggsave("/tmp/histo_slopes.pdf")
-  echo "Mean of slopes ", df["slopes", float].mean
+    ggsave(&"/tmp/histo_slopes_{$suffix}.pdf")
+  ggplot(df, aes("intersects")) +
+    geom_histogram(bins = 100) +
+    ggsave(&"/tmp/histo_intersects{$suffix}.pdf")
+  ggplot(df, aes("slopes", "intersects")) +
+    geom_point() +
+    ggsave(&"/tmp/slope_vs_intersects{$suffix}.png", width = 1200, height = 800)
 
+  echo "Mean of slopes ", df["slopes", float].mean
   echo result
   ggplot(result, aes("xs", "ys", color = "ids")) +
     geom_line() +
-    ggsave("/tmp/lines.png", width = 4000, height = 2000)
+    ggsave(&"/tmp/lines{$suffix}.png", width = 4000, height = 2000)
 
-proc main(fname: string) =
+proc main(fname: string, suffix: int) =
   let df = readcsv(fname)
     .arrange(["x", "y"])
   #discard hashtest(df["x", int], df["y", int])
-  discard houghTrafo(df["x", int], df["y", int])
+  discard houghTrafo(df["x", int], df["y", int], suffix)
   ggplot(df, aes(x, y, color = charge)) +
     geom_point(size = some(1.0)) +
     xlim(0, 768) + ylim(0, 768) + scale_x_continuous() + scale_y_continuous() +
@@ -79,7 +88,7 @@ proc main(fname: string) =
     geom_linerange(aes = aes(x = 384, yMin = 512, yMax = 768)) +
     geom_linerange(aes = aes(x = 640, yMin = 512, yMax = 768)) +
     margin(top = 1.5) +
-    ggsave("/tmp/plot_s.pdf")
+    ggsave(&"/tmp/plot_septem_{$suffix}.pdf")
 
 when isMainModule:
   dispatch main

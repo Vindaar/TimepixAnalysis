9af8823dc735a4e670732b2e5ae6f885d5bf5f46
Vindaar <basti90@gmail.com>
2022-02-23 17:44:30 +0100

    add note about extending `getFileInfo` to auto determine base group
    The base group is trivial to figure out on our own as there are only a
    fixed number of them. A real `getFileInfo` shouldn't have to be told
    what to read from.

--- Analysis/ingrid/private/hdf5_utils.nim
+++ Analysis/ingrid/private/hdf5_utils.nim
@@ -702,6 +702,10 @@ proc timepixVersion*(h5f: H5File): TimepixVersion =
 
 proc getFileInfo*(h5f: H5File, baseGroup = recoGroupGrpStr()): FileInfo =
   ## returns a set of all run numbers in the given file
+
+  ## XXX: Extend this to automatically determine the base group! There's only 3 different cases
+  ## and we could do that automatically and store it in the `FileInfo`. From there we wouldn't
+  ## have to worry about it anymore!
   # visit file
   #h5f.visitFile()
   var readAux = false

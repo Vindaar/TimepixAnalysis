5933d35968aab47d93861d2f75bf9c57e2850de5
Vindaar <basti90@gmail.com>
2022-02-26 18:20:27 +0100

    [tools] make `printXYdataset` work more generally (different types)

--- Tools/printXyDataset.nim
+++ Tools/printXyDataset.nim
@@ -1,10 +1,26 @@
 import nimhdf5, cligen, strformat, os
 
-proc main(fname: string, chip: int, run: int) =
+proc main(fname: string, chip: int, run: int, dset: string,
+          reco = false, head = 0) =
   let h5f = H5open(fname, "r")
-
-  let grp = h5f[(&"/runs/run_{run}/chip_{chip}/").grp_str]
-  echo h5f[grp.name / "raw_x", seq[uint8]]
+  var path = "/runs"
+  if reco:
+    path = "/reconstruction"
+  let grp = h5f[(&"{path}/run_{run}/chip_{chip}/").grp_str]
+  let h5dset = h5f[(grp.name / dset).dset_str]
+  template readit(typ: untyped): untyped =
+    if head > 0:
+      echo h5Dset.readVlen(typ)[0 ..< head]
+    else:
+      echo h5Dset.readVlen(typ)
+  case h5dset.dtypeBaseKind
+  of dkUint8: readit(uint8)
+  of dkUint16: readit(uint16)
+  of dkUint64: readit(uint64)
+  of dkInt: readit(int)
+  of dkFloat: readit(float)
+  else:
+    echo "Unsupported data type kind ", h5dset.dtype
 
 when isMainModule:
   dispatch main

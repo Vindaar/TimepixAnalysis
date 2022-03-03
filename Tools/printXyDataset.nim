a6d66c57874ef04de2e9791badd8d74be50a7f85
Vindaar <basti90@gmail.com>
2022-03-03 18:29:24 +0100

    [tools] allow to print likelihood dsets w/ printXYDataset

--- Tools/printXyDataset.nim
+++ Tools/printXyDataset.nim
@@ -1,11 +1,13 @@
 import nimhdf5, cligen, strformat, os
 
 proc main(fname: string, chip: int, run: int, dset: string,
-          reco = false, head = 0) =
+          reco = false, likelihood = false, head = 0) =
   let h5f = H5open(fname, "r")
   var path = "/runs"
   if reco:
     path = "/reconstruction"
+  if likelihood:
+    path = "/likelihood"
   let grp = h5f[(&"{path}/run_{run}/chip_{chip}/").grp_str]
   let h5dset = h5f[(grp.name / dset).dset_str]
   template readit(typ: untyped): untyped =

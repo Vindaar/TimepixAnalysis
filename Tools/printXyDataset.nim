b5731b9436c335364134a330eeb63a0ac2191440
Vindaar <basti90@gmail.com>
2022-03-13 16:36:44 +0100

    [tools] add aggregate info output to `printXYDataset`

--- Tools/printXyDataset.nim
+++ Tools/printXyDataset.nim
@@ -1,7 +1,7 @@
-import nimhdf5, cligen, strformat, os
+import nimhdf5, cligen, strformat, os, seqmath, stats, sequtils
 
 proc main(fname: string, chip: int, run: int, dset: string,
-          reco = false, likelihood = false, head = 0) =
+          reco = false, likelihood = false, head = 0, verbose = false) =
   let h5f = H5open(fname, "r")
   var path = "/runs"
   if reco:
@@ -12,10 +12,28 @@ proc main(fname: string, chip: int, run: int, dset: string,
   let h5dset = h5f[(grp.name / dset).dset_str]
   echo "Dataset: ", dset
   withDset(h5dset):
+    var dsetL = dset
     if head > 0:
-      echo dset[0 ..< head]
+      dsetL = dsetL[0 ..< head]
+    if verbose:
+      echo dsetL
+    when typeof(dsetL[0]) is SomeNumber:
+      let dsetF = dsetL.mapIt(it.float)
+      echo "Min: ", dsetF.min
+      echo "Max: ", dsetF.max
+      echo "Mean: ", dsetF.mean
+      echo "Sum: ", dsetF.sum
+    elif typeof(dsetL[0]) is seq:
+      when typeof(dsetL[0][0]) is SomeNumber:
+        let dsetF = dsetL.flatten.mapIt(it.float)
+        echo "Min: ", dsetF.min
+        echo "Max: ", dsetF.max
+        echo "Mean: ", dsetF.mean
+        echo "Sum: ", dsetF.sum
     else:
-      echo dset
+      echo "No aggregates for type: ", typeof(dset)
+
+
 
 when isMainModule:
   dispatch main

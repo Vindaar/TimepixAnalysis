a3c927c268549aa55a166278444d71e3114d337f
Vindaar <basti90@gmail.com>
2022-05-24 10:54:02 +0200

    [tools] extend `printXyDataset` to optionally work on all runs in file

--- Tools/printXyDataset.nim
+++ Tools/printXyDataset.nim
@@ -1,16 +1,23 @@
 import nimhdf5, cligen, seqmath
 import std / [strformat, os, stats, sequtils, sets, algorithm, strutils]
+import ingrid / tos_helpers
 
 type
   ## A generic cut on input data using dset & low / high values
   GenericCut* = tuple[dset: string, lower, upper: float]
 
-proc readIndices(h5f: H5File, path, dset: string, cuts: seq[GenericCut]): seq[int] =
+  Stats = object
+    min: float
+    max: float
+    mean: float
+    sum: float
+
+proc readIndices(h5f: H5File, path: string, cuts: seq[GenericCut]): seq[int] =
   ## get the correct indices we need to read for the data `dset` such that it matches
   ## the given cuts. Cuts are only allowed from datasets at the same level as `path`
   ## and must be readable as `float`.
   # 1. get `dset`
-  let dset = h5f[(path / dset).dset_str]
+  let dset = h5f[path.dset_str]
   # 2. get number of elements from shape of dset
   let num = dset.shape[0]
   # 3. create full set of indices from shape (i.e. all data)
@@ -25,19 +32,10 @@ proc readIndices(h5f: H5File, path, dset: string, cuts: seq[GenericCut]): seq[in
         idxs.excl i
   result = toSeq(idxs).sorted
 
-proc main(fname: string, chip: int, run: int, dset: string,
-          reco = false, likelihood = false, head = 0, verbose = false,
-          cuts: seq[GenericCut] = @[]) =
-  let h5f = H5open(fname, "r")
-  var path = "/runs"
-  if reco:
-    path = "/reconstruction"
-  if likelihood:
-    path = "/likelihood"
-  let grpPath = &"{path}/run_{run}/chip_{chip}/"
-  let h5dset = h5f[(grpPath / dset).dset_str]
-  let idxs = readIndices(h5f, grpPath, dset, cuts)
-  echo "Dataset: ", dset
+proc singleRun(h5f: H5File, path: string,
+               head: int, verbose: bool, cuts: seq[GenericCut] = @[]): Stats =
+  let h5dset = h5f[path.dset_str]
+  let idxs = readIndices(h5f, path, cuts)
   withDset(h5dset):
     var dsetL = idxs.mapIt(dset[it])
     if head > 0:
@@ -46,19 +44,62 @@ proc main(fname: string, chip: int, run: int, dset: string,
       echo dsetL
     when typeof(dsetL[0]) is SomeNumber:
       let dsetF = dsetL.mapIt(it.float)
-      echo "Min: ", dsetF.min
-      echo "Max: ", dsetF.max
-      echo "Mean: ", dsetF.mean
-      echo "Sum: ", dsetF.sum
+      result = Stats(min: dsetF.min, max: dsetF.max,
+                     mean: dsetF.mean, sum: dsetF.sum)
+      echo "Min: ", result.min
+      echo "Max: ", result.max
+      echo "Mean: ", result.mean
+      echo "Sum: ", result.sum
     elif typeof(dsetL[0]) is seq:
       when typeof(dsetL[0][0]) is SomeNumber:
         let dsetF = dsetL.flatten.mapIt(it.float)
-        echo "Min: ", dsetF.min
-        echo "Max: ", dsetF.max
-        echo "Mean: ", dsetF.mean
-        echo "Sum: ", dsetF.sum
+        result = Stats(min: dsetF.min, max: dsetF.max,
+                       mean: dsetF.mean, sum: dsetF.sum)
+        echo "Min: ", result.min
+        echo "Max: ", result.max
+        echo "Mean: ", result.mean
+        echo "Sum: ", result.sum
     else:
       echo "No aggregates for type: ", typeof(dset)
+
+proc getPath(base: string, run: int, chip: int, dset: string, global: bool): string =
+  result = ""
+  if not global:
+    if chip < 0:
+      quit("For chip related datasets, must hand a chip number!")
+    result = &"{base}{run}/chip_{chip}/{dset}"
+  else:
+    result = &"{base}{run}/{dset}"
+
+proc main(fname: string, dset:string, global = false,
+          run = -1, chip = -1,
+          reco = false, likelihood = false, head = 0, verbose = false,
+          cuts: seq[GenericCut] = @[]) =
+  let h5f = H5open(fname, "r")
+  var path = rawDataBase()
+  if reco:
+    path = recoBase()
+  if likelihood:
+    path = likelihoodBase()
+
+  echo "Dataset: ", dset
+  if run >= 0:
+    let dsetPath = getPath(path, run, chip, dset, global)
+    discard singleRun(h5f, dsetPath, head, verbose, cuts)
+  else:
+    var stats = newSeq[Stats]()
+    for (num, grp) in runs(h5f, path):
+      echo "Run: ", num
+      echo "--------------------------------------------------"
+      let dsetPath = getPath(path, num, chip, dset, global)
+      stats.add singleRun(h5f, dsetPath, head, verbose, cuts)
+    echo "=================================================="
+    echo "Combined stats:"
+    echo "=================================================="
+    echo "Min: ", stats.mapIt(it.min).min
+    echo "Max: ", stats.mapIt(it.max).max
+    echo "Mean: ", stats.mapIt(it.mean).mean
+    echo "Sum: ", stats.mapIt(it.sum).sum
 
 when isMainModule:
   import cligen/argcvt

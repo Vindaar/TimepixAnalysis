a69fd544903f91dbb84789292726ca66e1399db9
Vindaar <basti90@gmail.com>
2023-01-20 14:42:40 +0100

    [tools] make opt gas gain slice more generic, add more plots
    This is a big change to the `optimizeGasGainSliceTime` tool for the
    thesis. It first of all makes it more generic to work on any device
    that has the required data files, an input path is now required.
    
    Further, it adds many plots that make understanding the results
    nicer, including discussions of goodness of fit tests on the median /
    variance of the resulting distributions.

--- Tools/optimizeGasGainSliceTime.nim
+++ Tools/optimizeGasGainSliceTime.nim
@@ -1,5 +1,5 @@
-import shell, parsetoml, strutils, nimhdf5, os, times, strformat
-import ggplotnim
+import shell, strutils, nimhdf5, os, times, strformat, tables, algorithm, sugar
+import ggplotnim, stats
 
 import ingrid / [tos_helpers, calibration, ingrid_types]
 
@@ -48,10 +48,10 @@ CSV for the existing 30 min stored in the files first.
 ]#
 
 const timeIntervals = [45, 60, 90, 120, 180, 300]
-const Data2017 = "/mnt/1TB/CAST/2017/DataRuns2017_Reco.h5"
-const Data2018 = "/mnt/1TB/CAST/2018_2/DataRuns2018_Reco.h5"
+const Data2017 = "DataRuns2017_Reco.h5"
+const Data2018 = "DataRuns2018_Reco.h5"
 
-proc percentile[T](t: Tensor[T], perc: float): float =
+proc percentile[T: SomeNumber](t: Tensor[T], perc: float): float =
   let dataSorted = t.sorted
   let perIdx = min((t.size.float * perc).round.int, t.size - 1)
   result = dataSorted[perIdx]
@@ -104,12 +104,25 @@ proc computeMedianEnergy(f: string, interval: int): DataFrame =
   result = result.arrange("timestamp")
   discard h5f.close()
 
-proc computeAndWriteFinalDf(interval: int) =
+proc findFile(p: string, f: string): string =
+  ## Returns the path to the file `f` if it exists in one of two allowed places, else
+  ## raises.
+  if existsFile(p / f):
+    result = p / f
+  elif existsFile(p / "2017" / f):
+    result = p / "2017" / f
+  elif existsFile(p / "2018_2" / f):
+    result = p / "2018_2" / f
+  else:
+    raise newException(IOError, "The given path `" & $p & "` does not contain " & $f)
+
+proc computeAndWriteFinalDf(path: string, interval: int) =
   var df = newDataFrame()
   for f in [Data2017, Data2018]:
-    df.add computeMedianEnergy(f, interval)
+    df.add computeMedianEnergy(path.findFile(f), interval)
   df = df.splitDf()
-  df.write_csv(&"out/median_energies_interval_{interval}.csv")
+  df.mutate(fn {float -> int: "timestamp" ~ `timestamp`.int})
+    .write_csv(&"out/median_energies_interval_{interval}.csv")
 
 #computeAndWriteFinalDf(30)
 when false:
@@ -128,71 +141,156 @@ proc computeNewGainAndEnergies(arg: tuple[f: string, interval: int]) {.thread.} 
   if code != 0:
     raise newException(Exception, "Error running the given command for " & $f)
 
-proc generateCsv() =
+proc generateCsv(path: string) =
   createDir("out")
   var thr: array[2, Thread[tuple[f: string, interval: int]]]
   for i, t in timeIntervals:
     #if i == 0:
     #  # need to handle currently existing interval
     #  computeAndWriteFinalDf(30)
+    ## XXX: replace hardcoded path here! Use CT TPA introspection module I need to write!
+    ## Alternative would be a custom config file that we provide the `reconstruction` call with!
     rewriteToml("/home/basti/CastData/ExternCode/TimepixAnalysis/Analysis/ingrid/config.toml", t)
-    createThread(thr[0], computeNewGainAndEnergies, (Data2017, t))
-    createThread(thr[1], computeNewGainAndEnergies, (Data2018, t))
+    createThread(thr[0], computeNewGainAndEnergies, (path.findFile(Data2017), t))
+    createThread(thr[1], computeNewGainAndEnergies, (path.findFile(Data2018), t))
     joinThreads(thr)
 
-    computeAndWriteFinalDf(t)
+    path.computeAndWriteFinalDf(t)
 
 proc fromFile(f: string): DataFrame =
-  result = toDf(readCsv(f))
+  result = readCsv(f)
   let interval = f.extractFilename.split(".")[0].split("_")[^1].parseInt
   result["Interval"] = constantColumn(interval, result.len)
 
-proc plot() =
+
+import fitl / gof
+import arraymancer / stats / kde
+proc plot(path: string) =
   var df = newDataFrame()
-  for (pcKind, f) in walkDir("out"):
-    case pcKind
-    of pcFile: df.add fromFile(f)
-    else: echo "Skipping ", f, " of kind ", pcKind
+  for f in walkFiles(path / "*.csv"):
+    df.add fromFile(f)
   echo df
   df = df.mutate(fn {"medianEnergy" ~ `medianEnergy` * 1e6})
     .filter(fn {`medianEnergy` < 5.0})
   const bins = 150
 
-  ggplot(df, aes("medianEnergy", color = "Interval")) +
-    facet_wrap("RunPeriod", scales = "free") +
-    geom_histogram(bins = bins,
-                   lineWidth = some(1.25),
-                   alpha = some(0.0),
-                   hdKind = hdOutline,
-                   # density = true
-                   position = "identity") +
-    xlim(2, 5) +
-    ggtitle("Different interval lengths for the gas gain computation in minutes") +
-    ggsave("/tmp/medianEnergy_intervals.pdf", width = 1200, height = 800)
+  let labOrd = collect:
+    for i, x in timeIntervals.sorted(SortOrder.Descending):
+      { %~ x : i }
+  echo labOrd
+  let xlab = "Median energy [keV]"
+  df = df.rename(f{xlab <- "medianEnergy"})
+
+  # start with ridgeline and individual histograms of each period
   for (tup, subDf) in groups(df.group_by("RunPeriod")):
     let period = tup[0][1].toStr.replace("/", "_")
-    ggplot(subDf, aes("medianEnergy", fill = "Interval")) +
-      ggridges("Interval", overlap = 1.0) +
-      geom_histogram(bins = bins,
+    ## XXX: fix code to work with different label order!!
+    #let subDf = subDf.filter(fn {float: idx("medianEnergy") >= percentile(col("medianEnergy"), 0.01) and
+    #                             idx("medianEnergy") <= percentile(col("medianEnergy"), 0.99)})
+    let ridgeBins = 100
+    ggplot(subDf, aes(xlab, fill = factor("Interval"))) +
+      ggridges("Interval", overlap = 2.0) + # , labelOrder = labOrd) +
+      geom_histogram(bins = ridgeBins,
                      hdKind = hdOutline,
-                     position = "identity") +
-      ggtitle("Different interval lengths for the gas gain computation in minutes") +
+                     position = "identity",
+                     density = true,
+                     color = "black", lineWidth = 2.0) +
+      #yMargin(0.2) +
+      ggtitle(&"Different interval lengths for the gas gain computation in minutes, data starting {tup[0][1].toStr}") +
       ggsave(&"/tmp/medianEnergy_ridges_{period}.pdf", width = 1200, height = 800)
 
+    ggplot(subDf, aes(xlab, color = factor("Interval"))) +
+      geom_histogram(bins = 40,
+                     lineWidth = some(2.25),
+                     alpha = some(0.0),
+                     hdKind = hdOutline,
+                     density = true,
+                     position = "identity") +
+      #xlim(2, 5) +
+      ggtitle("Different interval lengths for the gas gain computation in minutes") +
+      ggsave(&"/tmp/medianEnergy_intervals_{period}.pdf", width = 1200, height = 800)
+
+  # and the individual plots
   for (tup, subDf) in groups(df.group_by("Interval")):
     let suff = tup[0][1].toStr
-    ggplot(subDf, aes("timestamp", "medianEnergy")) +
+    ggplot(subDf, aes("timestamp", xlab)) +
       facet_wrap("RunPeriod", scales = "free") +
       geom_point(alpha = some(0.7)) +
       ggtitle(&"Median energy vs time {suff}") +
       ggsave(&"/tmp/medianEnergy_vs_time_{suff}.pdf", width = 1200, height = 800)
 
+  # next plot the data as histograms in a facet
+  let dfF = df.group_by("Interval")
+    .filter(fn {float: idx(xlab) >= percentile(col(xlab), 0.01) and
+                idx(xlab) <= percentile(col(xlab), 0.99)})
+  ggplot(dfF, aes(xlab, color = factor("Interval"))) +
+    facet_wrap("RunPeriod", scales = "free") +
+    geom_histogram(bins = 30,
+                   lineWidth = some(2.0),
+                   alpha = some(0.0),
+                   hdKind = hdOutline,
+                   density = true,
+                   position = "identity") +
+    xlim(2, 5) +
+    ggtitle("Different interval lengths for the gas gain computation in minutes") +
+    ggsave("/tmp/medianEnergy_intervals.pdf", width = 1200, height = 800)
 
-proc main(genCsv: bool = false, plot: bool = false) =
+  # and finally using a KDE approach
+  var dfK = newDataFrame()
+  let samples = linspace(2.0, 4.0, 1000).toTensor
+  for (tup, subDf) in groups(df.group_by(["Interval", "RunPeriod"])):
+    let data = subDf[xlab, float]
+    let kde = kde(data, samples = samples)
+    var dfLoc = toDf({"Median energy [keV]" : samples, "Density" : kde, "RunPeriod" : tup[1][1].toStr, "Interval" : tup[0][1].toInt})
+    dfK.add dfLoc
+
+  ggplot(dfK, aes(xlab, "Density", color = factor("Interval"))) +
+    facet_wrap("RunPeriod", scales = "free") +
+    geom_line() +
+    ggtitle("Different interval lengths for the gas gain computation in minutes") +
+    ggsave("/tmp/medianEnergy_kde_intervals.pdf", width = 1200, height = 800)
+
+  for (tup, subDf) in groups(dfK.group_by("RunPeriod")):
+    let period = tup[0][1].toStr.replace("/", "_")
+    ggplot(subDf, aes(xlab, "Density", fill = factor("Interval"))) +
+      ggridges("Interval", overlap = 2.0) + # , labelOrder = labOrd) +
+      geom_line(color = "black", size = 2.0) +
+      ggtitle(&"Different interval lengths for the gas gain computation in minutes, data starting {tup[0][1].toStr}") +
+      ggsave(&"/tmp/medianEnergy_kde_ridges_{period}.pdf", width = 1200, height = 800)
+
+  # As a bonus compute GoF tests for the data in each case and create a comparison plot.
+  block Tests:
+    let allMean = df[xlab, float].mean
+    let allVar = df[xlab, float].variance
+    var tests = newSeq[string]()
+    var gofs = newSeq[float]()
+    var ints = newSeq[int]()
+    var pers = newSeq[string]()
+    for test in GoFTest:
+      for (tup, subDf) in groups(df.group_by(["Interval", "RunPeriod"])):
+        let dat = subDf[xlab, float].toSeq1D
+        let gof = gofStat(dat, (allMean, allVar), test)
+        echo tup, ", ", test, " = ", gof
+        tests.add $test
+        gofs.add gof
+        ints.add tup[0][1].toInt
+        pers.add tup[1][1].toStr
+    let dfT = toDf({"Goodness of fit test" : tests, "GoF value" : gofs, "Interval" : ints, "RunPeriod" : pers})
+    ggplot(dfT, aes("Goodness of fit test", "GoF value", color = "Interval", shape = "RunPeriod")) +
+      geom_point() +
+      scale_y_log10() +
+      ggtitle("Goodness of fit tests for median energy data by period & interval length") +
+      ggsave("/t/gofs_for_different_binnings.pdf", width = 800, height = 600)
+
+proc main(path: string, genCsv: bool = false, plot: bool = false) =
+  ## The given `path` is either a path to a place that contains both `DataRuns` H5 files
+  ## (potentially in 2017, 2018_2 subdirectories) or a path to a directory containing CSV
+  ## files that are generated from the `genCsv` call. Note that in the latter case the
+  ## directory *must not* contain any other CSV files!
   if genCsv:
-    generateCsv()
+    generateCsv(path)
   if plot:
-    plot()
+    plot(path)
 
 import cligen
 when isMainModule:

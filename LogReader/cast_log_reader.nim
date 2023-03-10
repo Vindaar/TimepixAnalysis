85d6a0f0475281994714aea78a5784ef6f22bb48
Vindaar <basti90@gmail.com>
2023-03-10 21:47:38 +0100

    [LogReader] add error message for a quit condition

--- LogReader/cast_log_reader.nim
+++ LogReader/cast_log_reader.nim
@@ -853,6 +853,7 @@ proc parse_tracking_logfile*(content: string, filename: string): TrackingLog =
   result.name = filename
   result.badLineCount = badLineCount
   if result.tracking_stop < parseTime("1975/01/01", "YYYY/MM/dd", utc()):
+    echo "[ERROR]: Invalid tracking stop time parsed: ", result
     quit()
 
 proc read_tracking_logfile*(filename: string): TrackingLog =

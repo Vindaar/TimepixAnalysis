84d7cc9c74083030409ea26a855b37ea6f2f4ddf
Vindaar <basti90@gmail.com>
2024-09-13 10:21:21 +0200

    [limit] interpolate background, split Context into Context + Config
    The Config is a fully compile 'time evaluatable' object for the help screen.

--- Analysis/ingrid/basic_limit_calc.nim
+++ Analysis/ingrid/basic_limit_calc.nim
@@ -13,6 +13,8 @@ defUnit(GeV⁻²)
 defUnit(cm⁻²•s⁻¹)
 defUnit(keV⁻¹)
 defUnit(keV⁻¹•cm⁻²•s⁻¹)
+defUnit(mm²)
+defUnit(cm²)
 
 ##[
 Important note:
@@ -26,17 +28,20 @@ units.
 ]##
 
 type
-  Context = object
+  Config = object # Stores experimental configuration
     B*: Tesla ## Magnetic field of the magnet
     L*: Meter ## Length of the magnet
     totalTime*: Hour ## Total time in hours of solar tracking
     boreRadius*: cm ## Radius of the bore (assumes a circular bore, A = πr²)
     chipArea*: mm² ## Area in which all flux is assumed to be collected and in which
                    ## all candidates are detected & background is defined
-    #
+    areaBore: cm²  ## Full bore area, computed from `boreRadius`
+
+  Context = object
+    cfg: Config
     fluxIntegral: cm⁻²•s⁻¹ ## the base integral of the flux over the entire energy
     # Interpolators
-
+    background: InterpolatorType[keV⁻¹•cm⁻²•s⁻¹]
 
 ## Constants defining the channels and background info
 const
@@ -44,21 +49,33 @@ const
   Background = @[0.5e-5, 2.5e-5, 4.5e-5, 4.0e-5, 1.0e-5, 0.75e-5, 0.8e-5, 3e-5, 3.5e-5, 2.0e-5]
     .mapIt(it.keV⁻¹•cm⁻²•s⁻¹) # convert to a rate
   ## A possible set of candidates from `Background · chipArea · totalTime · 1 keV`
-  ## (1e-5 · 5x5mm² · 100h = 0.9 counts)
+  ## (1e-5 · 5x5mm² · 100h = 0.9 counts•keV⁻¹)
   Candidates = @[0,      2,      7,     3,      1,      0,       1,      4,    3,      2]
 
-proc solarAxionFlux(ω: keV): keV⁻¹•cm⁻²•s⁻¹
+proc solarAxionFlux(ω: keV): keV⁻¹•cm⁻²•s⁻¹ # forward declare
 
-proc initContext(): Context =
+proc initConfig(): Config =
+  ## This stores all 'experimental configuration' parameter. These are all constant and
+  ## don't change for one run of the program.
+  let bR = 70.cm
+  result = Config(B: 3.5.Tesla,
+                   L: 20.m,
+                   totalTime: 100.h,
+                   boreRadius: bR,
+                   areaBore: π * bR^2)
+
+proc initContext(cfg: Config): Context =
   # 1. integrate the solar flux
   ## NOTE: in practice this integration must not be done in this proc! Only perform once!
   let xs = linspace(0.0, 10.0, 1000)
   let fl = xs.mapIt(solarAxionFlux(it.keV))
   let integral = simpson(fl.mapIt(it.float), # convert units to float for compatibility
                          xs).cm⁻²•s⁻¹ # convert back to units (integrated out `keV⁻¹`!)
-  result = Context(B: 3.5.Tesla,
-                   L: 20.m,
-                   fluxIntegral: integral)
+  # 2. construct background interpolator
+  let bkg = newLinear1D(Energies.mapIt(it.float), Background)
+  result = Context(cfg: cfg,
+                   fluxIntegral: integral,
+                   background: bkg)
 
 proc solarAxionFlux(ω: keV): keV⁻¹•cm⁻²•s⁻¹ =
   ## Axion flux produced by the Primakoff effect in solar core
@@ -76,7 +93,7 @@ func conversionProbability(ctx: Context): UnitLess =
   ##
   ## This is computed *WITHOUT* `g_aγ` present, i.e. we set `g_aγ = 1 GeV⁻¹`, so that
   ## in `signal` we multiply only by `g_aγ⁴` (to simplify between `g_ae` and `g_aγ`).
-  result = pow( (1.GeV⁻¹ * ctx.B.toNaturalUnit * ctx.L.toNaturalUnit / 2.0), 2.0 )
+  result = pow( (1.GeV⁻¹ * ctx.cfg.B.toNaturalUnit * ctx.cfg.L.toNaturalUnit / 2.0), 2.0 )
 
 from numericalnim import simpson # simpson numerical integration routine
 proc totalFlux(ctx: Context, g²: float): float =
@@ -84,24 +101,20 @@ proc totalFlux(ctx: Context, g²: float): float =
   # 2. compute final flux by "integrating" out the time and area
   ## Multiply by `g²^2`. `g²` can be either `g_ae·g_aγ` or `g_aγ²`. We square that
   ## so that we multiply by the 'missing' terms in both P_aγ and the flux.
-  result = ctx.fluxIntegral * totalTime * areaBore * ctx.conversionProbability() * g²^2
+  result = ctx.fluxIntegral * totalTime * ctx.cfg.areaBore * ctx.conversionProbability() * g²^2
 
 ## NOTE: only important that signal and background have the same units!
 proc signal(ctx: Context, E: keV, g²: float): keV⁻¹ =
   ## Returns the axion flux based on `g` and energy `E`
-  result = solarAxionFlux(E) * totalTime.to(Second) * areaBore * ctx.conversionProbability() * g²^2
+  result = solarAxionFlux(E) * totalTime.to(Second) * ctx.cfg.areaBore * ctx.conversionProbability() * g²^2
 
-proc background(E: keV): keV⁻¹ =
-  ## Compute an interpolation of energies and background
-  ## NOTE: For simplicity we only evaluate at the channel energies anyway. In practice
-  ## one likely wants interpolation to handle all energies in the allowed range correctly!
-
-  ## XXX: Interpolate
-  let idx = Energies.lowerBound(E) # get idx of this energy
+proc background(ctx: Context, E: keV): keV⁻¹ =
+  ## Compute an interpolation the background at the energy `E`.
+  ##
   ## Note: area of interest is the region on the chip, in which the signal is focused!
   ## This also allows us to see that the "closer" we cut to the expected axion signal on the
   ## detector, the less background we have compared to the *fixed* signal flux!
-  result = (Background[idx] * totalTime * chipArea).to(keV⁻¹)
+  result = (ctx.background.eval(E.float).keV⁻¹•cm⁻²•s⁻¹ * ctx.cfg.totalTime * ctx.cfg.chipArea).to(keV⁻¹)
 
 proc likelihood(ctx: Context, g²: float, energies: seq[keV], cs: seq[int]): float =
   ## `energies` = energies corresponding to each channel
@@ -111,8 +124,8 @@ proc likelihood(ctx: Context, g²: float, energies: seq[keV], cs: seq[int]): flo
     let c = cs[i]       # number of candidates in this channel
     let E = energies[i] # energy of this channel
     let s = ctx.signal(E, g²)
-    let b = background(E)
-    result *= pow(1 + ctx.signal(E, g²) / background(E), c.float)
+    let b = ctx.background(E)
+    result *= pow(1 + s / b, c.float)
 
 proc computeLimit(ctx: Context): float =
   let xLin = linspace(0.0, 1e-20, 1000) # g²
@@ -149,11 +162,12 @@ when isMainModule:
   include mergeCfgEnvLocal
   include unchained/cligenParseUnits
 
-  const dfl* = initContext() # set defaults!=default for type
+  const dfl* = initConfig() # set defaults!=default for type
   var app = initFromCL(dfl, cmdName = "foo", help = {
     "B": "Magnetic field strength of the magnet",
-    "L": "Magnet length in meters"
+    "L": "Magnet length in meters",
+    "boreRadius" : "Radius of the magnet bore",
+    "totalTime" : "Total solar tracking time"
   })
-  app.main # Only --help/--version/parse errors cause early exit
-
-  #dispatch main
+  let ctx = initContext(app)
+  ctx.main # Only --help/--version/parse errors cause early exit
